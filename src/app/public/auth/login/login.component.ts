4b3fce679010d67ec1a3a653691839b41320899b
Jhon Pérez <jhon.perez@pragma.com.co>
2018-08-17 10:59:08 -0500

    Remove coments in code

--- src/app/public/auth/login/login.component.ts
+++ src/app/public/auth/login/login.component.ts
@@ -59,6 +59,7 @@ export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit
     awscognitogroup: FormGroup;
     // Define si la app esta en un entorno de producción.
     isProductionEnv = environment.production;
+    public consts = RoutesConst;
     // Variables del uso de aws-cognito
     email: string;
     password: string;
@@ -114,7 +115,6 @@ export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit
         }
         this.errorMessage = null;
         this.shell.loadingComponent.closeLoadingProgressBar();
-        console.log('LoginComponent');
         this.userService.authenticate(this.email, this.password, this);
         this.shell.loadingComponent.viewLoadingSpinner();
     }
@@ -125,10 +125,10 @@ export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit
             this.errorMessage = message;
             console.log('result: ' + this.errorMessage);
             if (this.errorMessage === 'User is not confirmed.') {
-                this.router.navigate([`/${RoutesConst.homeConfirmRegistration}`, this.email]);
+                this.router.navigate([`/${this.consts.homeConfirmRegistration}`, this.email]);
             } else if (this.errorMessage === 'User needs to set password.') {
                 console.log('redirecting to set new password');
-                this.router.navigate([`/${RoutesConst.homeNewPassword}`]);
+                this.router.navigate([`/${this.consts.homeNewPassword}`]);
             }
         } else { // success
             this.ddb.writeLogEntry('login');
@@ -140,8 +140,6 @@ export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit
     callback() { }
 
     getDataUser() {
-        console.log('Este es this');
-        console.log(this);
         this.userParams.getUserData(this);
     }
 
@@ -150,9 +148,9 @@ export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit
         this.shell.user = this.user;
         this.shell.loadingComponent.closeLoadingSpinner();
         if (this.user.sellerProfile === 'seller') {
-            this.router.navigate([`/${RoutesConst.sellerCenterOrders}`]);
+            this.router.navigate([`/${this.consts.sellerCenterOrders}`]);
         } else if (this.user.sellerProfile === 'administrator') {
-            this.router.navigate([`/${RoutesConst.sellerCenterIntSellerRegister}`]);
+            this.router.navigate([`/${this.consts.sellerCenterIntSellerRegister}`]);
         }
     }
 
@@ -171,7 +169,7 @@ export class LoginComponent implements CognitoCallback, LoggedInCallback, OnInit
 
     isLoggedIn(message: string, isLoggedIn: boolean) {
         if (isLoggedIn) {
-            this.router.navigate([`/${RoutesConst.securehome}`]);
+            this.router.navigate([`/${this.consts.securehome}`]);
         }
     }
 
