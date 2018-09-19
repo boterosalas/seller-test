aa71b69ee1a049fdb730ef2fa4da96174ca4f396
Jhon Pérez <jhon.perez@pragma.com.co>
2018-09-18 19:18:47 -0500

    Add endpoints for production

--- api-endpoints.ts
+++ api-endpoints.ts
@@ -90,8 +90,8 @@ export const endpoints = {
       getOffers: 'https://dgu5y5h0u3.execute-api.us-east-1.amazonaws.com/offer-pdn/{params}',
       patchOffers: 'https://dgu5y5h0u3.execute-api.us-east-1.amazonaws.com/offer-pdn',
       // Historical
-      getHistoricalOffers: 'https://k19m329r85.execute-api.us-east-1.amazonaws.com/OfferHistorical',
-      downloadHistorical: 'https://wq8pypt9ra.execute-api.us-east-1.amazonaws.com/OfferDownloadHistorical',
+      getHistoricalOffers: 'https://k19m329r85.execute-api.us-east-1.amazonaws.com/offerHistorical-pdn/{params}',
+      downloadHistorical: 'https://wq8pypt9ra.execute-api.us-east-1.amazonaws.com/offerDownloadHistorical-pdn/{params}',
       // Products
       products: 'https://pb78swws90.execute-api.us-east-1.amazonaws.com/products-pdn'
     }
