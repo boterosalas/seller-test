752ad769ee3db93c01207abe51e4c8730a9d7aeb
Jhon Pérez <jhon.perez@pragma.com.co>
2018-10-10 13:47:12 -0500

    Wizard CreacionProdUnitario OK

--- src/app/secure/products/create-product-unit/component-process/component-process.component.ts
+++ src/app/secure/products/create-product-unit/component-process/component-process.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-component-process',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./component-process.component.scss']
 })
 export class ComponentProcessComponent implements OnInit {
+  isLinear = false;
+  /* eanCtrl: FormGroup;
+  categoryCtrl: FormGroup;
+  basicInfoCtrl: FormGroup;
+  especificCtrl: FormGroup;
+  imageCtrl: FormGroup; */
+  eanFormGroup: FormGroup;
+  categoryFormGroup: FormGroup;
+  basicInfoFormGroup: FormGroup;
+  especificFormGroup: FormGroup;
+  imageFormGroup: FormGroup;
+  options: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+  });
+}
 
   ngOnInit() {
+    this.eanFormGroup = this._formBuilder.group({
+      eanCtrl: ['', Validators.required]
+    });
+    this.categoryFormGroup = this._formBuilder.group({
+      categoryCtrl: ['', Validators.required]
+    });
+    this.basicInfoFormGroup = this._formBuilder.group({
+      basicInfoCtrl: ['', Validators.required]
+    });
+    this.especificFormGroup = this._formBuilder.group({
+      especificCtrl: ['', Validators.required]
+    });
+    this.imageFormGroup = this._formBuilder.group({
+      imageCtrl: ['', Validators.required]
+    });
   }
 
 }
