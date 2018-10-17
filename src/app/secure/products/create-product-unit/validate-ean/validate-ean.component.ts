f2099d9b7bd381738eb5401b9049674b1f4b23d0
Jhon Pérez <jhon.perez@pragma.com.co>
2018-10-17 09:10:38 -0500

    HU validacion EAn, ejemplo servicio

--- src/app/secure/products/create-product-unit/validate-ean/validate-ean.component.ts
+++ src/app/secure/products/create-product-unit/validate-ean/validate-ean.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { EanServicesService } from '../validate-ean/ean-services.service';
 
 @Component({
   selector: 'app-validate-ean',
@@ -9,9 +10,10 @@ import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms'
 export class ValidateEanComponent implements OnInit {
   options: FormGroup;
   eanGroup: FormGroup;
+  public infox;
   // constructor() { }
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private service: EanServicesService) {
     this.options = fb.group({
       hideRequired: false,
       floatLabel: 'auto',
@@ -22,5 +24,11 @@ export class ValidateEanComponent implements OnInit {
     this.eanGroup = this.fb.group({
       eanCtrl: ['', Validators.required]
     });
+  }
+
+  getEanServices() {
+    this.infox = this.service.validateEan().subscribe(res => {
+      console.log('this.infox', this.infox);
+    }, error => {console.log('Servicio no funciona')});
   }
 }
