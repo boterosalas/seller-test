b7f21e3ecaa875e54b20665adbafcd009fee04b1
luis echeverry <luis.echeverry@pragma.com.co>
2018-10-22 15:12:13 -0500

    52124- Maquetación de vista de información basica de productos.

--- src/app/secure/products/create-product-unit/basic-information/basic-information.component.ts
+++ src/app/secure/products/create-product-unit/basic-information/basic-information.component.ts
@@ -11,14 +11,22 @@ export class ProductBasicInfoComponent implements OnInit {
 
     /** Initialize variables */
     formBasicInfo: FormGroup;
+    packing: FormGroup;
+    product: FormGroup;
+    formKeyword: FormGroup;
+    keywords: string[] = [];
 
-    constructor() { }
-
-    ngOnInit() {
+    constructor() {
         this.createForm();
     }
 
+    ngOnInit() {
+    }
+
     private createForm(): void {
+        this.formKeyword = new FormGroup({
+            Keyword: new FormControl('', [])
+        });
         this.formBasicInfo = new FormGroup({
             Name: new FormControl('',
                 [
@@ -40,9 +48,58 @@ export class ProductBasicInfoComponent implements OnInit {
                 [
                     Validators.required
                 ]),
+            packing: new FormGroup({
+                HighPacking: new FormControl('',
+                    [
+                        Validators.required
+                    ]),
+                LongPacking: new FormControl('',
+                    [
+                        Validators.required
+                    ]),
+                WidthPacking: new FormControl('',
+                    [
+                        Validators.required
+                    ]),
+                WeightPacking: new FormControl('',
+                    [
+                        Validators.required
+                    ])
+            }),
+            product: new FormGroup({
+                HighProduct: new FormControl('',
+                    [
+                        Validators.required
+                    ]),
+                LongProduct: new FormControl('',
+                    [
+                        Validators.required
+                    ]),
+                WidthProduct: new FormControl('',
+                    [
+                        Validators.required
+                    ]),
+                WeightProduct: new FormControl('',
+                    [
+                        Validators.required
+                    ])
+            }),
+            Description: new FormControl('',
+                [
+                    Validators.required
+                ])
         });
+
+        console.log(this.formBasicInfo);
     }
 
     public saveBasicInfo(): void {
+    }
+
+    public sabeKeyword(): void {
+        if ( this.formKeyword.controls.Keyword.value) {
+            this.keywords.push(this.formKeyword.controls.Keyword.value);
+            this.formKeyword.controls.Keyword.setValue(null);
+        }
     }
 }
