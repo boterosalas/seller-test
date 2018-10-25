f7b8655519c4e735dbaa9f302a6061321750838f
JosefPK <josef.ban.v@gmail.com>
2018-10-24 23:32:52 -0500

    HU validacion EAN arreglo mat errors, falta, permitir continuar con checkbox

--- src/app/secure/products/create-product-unit/validate-ean/validate-ean.component.ts
+++ src/app/secure/products/create-product-unit/validate-ean/validate-ean.component.ts
@@ -22,6 +22,7 @@ export class ValidateEanComponent implements OnInit {
   eanGroup: FormGroup;
   public validateEanExist;
   public formatEan = /^(([a-zA-Z0-9]{7,13})|([0-9]{7,13}))$/;
+  public activeButtonCreacionUnitaria: boolean;
   // constructor() { }
 
   // metodo para chekear el chechk box
@@ -29,24 +30,46 @@ export class ValidateEanComponent implements OnInit {
   }
   ngOnInit() {
     // metodo para validar el input del form
+    console.log('validateEanExist: ', this.validateEanExist);
     this.eanGroup = this.fb.group({
       eanCtrl: ['', Validators.pattern(this.formatEan)],
       associateEan: false,
-      floatLabel: 'auto',
+      floatLabel: 'auto'
     });
+    this.validateEanExist = true;
+    console.log('this.validateEanExist: ', this.eanGroup.controls.eanCtrl.value);
   }
 
   // consumiendo servicio para validar si el EAN es valido
-  getEanServices() {
-    this.validateEanExist = false;
-    this.service.validateEan(this.eanGroup.controls.eanCtrl.value).subscribe(res => {
-      this.validateEanExist = !res['data'];
-      if ( !this.validateEanExist ) {
-        this.eanGroup.controls.eanCtrl.setErrors({ 'validExistEanDB': !this.validateEanExist});
-      }
-    }, error => {
-      this.validateEanExist = true;
-      console.log('Servicio no funciona');
-    });
+  validateEanServices(validateEanExist: any) {
+    this.activeButtonCreacionUnitaria = false;
+    console.log('input: ', this.eanGroup.value.eanCtrl);
+    if (this.eanGroup.value.eanCtrl.length >= 7 && this.eanGroup.value.eanCtrl.length <= 13) {
+      console.log('this.eanGroup.value.eanCtrl: ', this.eanGroup.value.eanCtrl.length);
+      this.service.validateEan(this.eanGroup.controls.eanCtrl.value).subscribe(res => {
+        // Validar si la data es un booleano
+        this.validateEanExist = (res['data']);
+        if (!!(res['data'] === true || !!(res['data'] === false))) {
+          console.log('validateEanExist 2: ', this.validateEanExist);
+          // throw new Error('Data not valid');
+        }
+        if (this.validateEanExist) {
+          this.eanGroup.controls.eanCtrl.setErrors({ 'validExistEanDB': this.validateEanExist });
+        }
+        if (!this.validateEanExist) {
+          this.activeButtonCreacionUnitaria = true;
+        }
+      }, error => {
+        // this.validateEanExist = true;
+        console.log('Servicio no funciona');
+      });
+    } else {
+      console.log('Campo invalido');
+    }
+  }
+
+// Metodo para deshabiliar boton continuar
+  disabledButtonUnitCreation() {
+    this.activeButtonCreacionUnitaria = false;
   }
 }
