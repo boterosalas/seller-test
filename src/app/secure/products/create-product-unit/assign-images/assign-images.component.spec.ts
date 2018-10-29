33efb1b69c91bbc77a2d17071d3392991a2638ff
JosefPK <josef.ban.v@gmail.com>
2018-10-29 10:57:55 -0500

    HU 52168 creacion componente, inicio

---
+++ src/app/secure/products/create-product-unit/assign-images/assign-images.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AssignImagesComponent } from './assign-images.component';
+
+describe('AssignImagesComponent', () => {
+  let component: AssignImagesComponent;
+  let fixture: ComponentFixture<AssignImagesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AssignImagesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AssignImagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
