ba2ab1ee17f9517c537eb75977697d94156f3219
luis echeverry <luis.echeverry@pragma.com.co>
2018-10-31 14:12:10 -0500

    52124 - Se realizan cambios a información básica de producto unitario.

--- src/app/secure/products/create-product-unit/basic-information/basic-information.component.ts
+++ src/app/secure/products/create-product-unit/basic-information/basic-information.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
-import { ColorPickerModule } from 'ngx-color-picker';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
     selector: 'app-basic-information',
@@ -17,19 +17,36 @@ export class ProductBasicInfoComponent implements OnInit {
     formKeyword: FormGroup;
     keywords: string[] = [];
     colorSelected: string;
+    sonList = [];
+    colorPick: string;
+    colorPick2: string;
+    ColorPDP: string = null;
+    CodeName: string = null;
 
     /**
      *  Json  con los colores predefinidos.
      */
     colorList = [
-        { name: 'Black', color: '#000000', border: '#000000' },
-        { name: 'White', color: '#FFFFFF', border: '#bdbdbd' },
-        { name: 'Red', color: '#F44336', border: '#c10000' },
-        { name: 'Yellow', color: '#FEEA3A', border: '#cece00' },
-        { name: 'Blue', color: '#03A9F4', border: '#8282f9' },
-        { name: 'Purple', color: '#6639B6', border: '#670167' }];
+        { Name: 'Beige', color: '#F5F5DC', border: '#e2e1c8' },
+        { Name: 'Negro', color: '#000000', border: '#000000' },
+        { Name: 'Blanco', color: '#FFFFFF', border: '#bdbdbd' },
+        { Name: 'Azul', color: '#03A9F4', border: '#8282f9' },
+        { Name: 'Amarillo', color: '#FEEA3A', border: '#cece00' },
+        { Name: 'Cafe', color: '#4E342E', border: '#3E2723' },
+        { Name: 'Gris', color: '#37474F', border: '#565656' },
+        { Name: 'Verde', color: '#2E7D32', border: '#1B5E20' },
+        { Name: 'Naranja', color: '#FF8F00', border: '#FF6F00' },
+        { Name: 'Rosa', color: '#E91E63', border: '#C2185B' },
+        { Name: 'Morado', color: '#6639B6', border: '#670167' },
+        { Name: 'Rojo', color: '#c62828', border: '#b71c1c' },
+        { Name: 'Plata', color: '#BDBDBD', border: '#9E9E9E' },
+        { Name: 'Dorado', color: '#FFB300', border: '#FFA000' },
+        { Name: 'MultiColor', color: '#FFB300', border: '#FFA000' },
+    ];
 
-    constructor() {
+    constructor(
+        private snackBar: MatSnackBar
+    ) {
         this.createForm();
     }
 
@@ -110,8 +127,13 @@ export class ProductBasicInfoComponent implements OnInit {
     }
 
     public sabeKeyword(): void {
-        if (this.formKeyword.controls.Keyword.value) {
-            this.keywords.push(this.formKeyword.controls.Keyword.value);
+        let word = this.formKeyword.controls.Keyword.value;
+        if (word) {
+            word = word.trim();
+            if (word[word.length] !== ',') {
+                word += ',';
+            }
+            this.keywords.push(word);
             this.formKeyword.controls.Keyword.setValue(null);
         }
     }
@@ -119,7 +141,7 @@ export class ProductBasicInfoComponent implements OnInit {
      * selectColor
      */
     public selectColor(color: any): void {
-        this.colorSelected = color.name;
+        this.colorSelected = color.Name;
         console.log(color);
     }
 
@@ -146,7 +168,6 @@ export class ProductBasicInfoComponent implements OnInit {
                 g = parseInt(hex.slice(2, 4), 16),
                 b = parseInt(hex.slice(4, 6), 16);
             if (bw) {
-                // http://stackoverflow.com/a/3943023/112731
                 return (r * 0.299 + g * 0.587 + b * 0.114) > 186
                     ? '#bdbdbd'
                     : '#FFFFFF';
@@ -155,4 +176,62 @@ export class ProductBasicInfoComponent implements OnInit {
             return '#' + this.padZero((255 - r).toString(16)) + this.padZero((255 - g).toString(16)) + this.padZero((255 - b).toString(16));
         }
     }
+
+    public addSon(): void {
+        if (this.validarNewSon()) {
+            const newForm = {
+                form: new FormGroup({
+                    Ean: new FormControl('',
+                        [
+                            Validators.required,
+                        ]),
+                    Size: new FormControl('',
+                        [
+                            Validators.required,
+                        ]),
+                    HexColorCodePDP: new FormControl('',
+                        [
+                            Validators.required,
+                        ]),
+                    HexColorCodeName: new FormControl('',
+                        [
+                            Validators.required,
+                        ])
+                }),
+                Show: true,
+                colorPick: null,
+                colorPick2: null,
+            };
+            let t = newForm.form.controls.HexColorCodePDP.disable();
+            t = newForm.form.controls.HexColorCodeName.disable();
+            this.sonList.push(newForm);
+        } else {
+            // error to show
+
+            this.snackBar.open('Para agregar mas hijos es necesario que el ultimo tenga todos los datos correctos', 'Cerrar', {
+                duration: 3000,
+            });
+        }
+    }
+
+    public validarNewSon(): boolean {
+        if (this.sonList.length) {
+            if (!this.sonList[this.sonList.length - 1].form.valid) {
+                this.sonList[this.sonList.length - 1].form.controls.Ean.markAsDirty();
+                this.sonList[this.sonList.length - 1].form.controls.Size.markAsDirty();
+                this.sonList[this.sonList.length - 1].form.controls.HexColorCodePDP.markAsDirty();
+                this.sonList[this.sonList.length - 1].form.controls.HexColorCodeName.markAsDirty();
+                console.log(this.sonList[this.sonList.length - 1].form.controls.HexColorCodePDP);
+            }
+            return this.sonList[this.sonList.length - 1].form.valid;
+        } else {
+            return true;
+        }
+    }
+
+
+    public toggleSon(son: any): void {
+        son.Show = !son.Show;
+    }
+
 }
