3e74d2b11533addb7f38832e5a6847a2335c041d
Jose Fernan <jose.banguera@pragma.com.co>
2019-02-28 09:15:21 -0500

    Expanded product 3

--- src/app/secure/products/list-products/expanded-product/expanded-product.component.ts
+++ src/app/secure/products/list-products/expanded-product/expanded-product.component.ts
@@ -10,9 +10,37 @@ const log = new Logger('ExpandedProductComponent');
 })
 export class ExpandedProductComponent implements OnInit {
 
+    /* arreglo q contiene las imagenes grandes y pequeñas */
+    private images: any;
+
+    /* variable que contiene la ruta de la imagen grande */
+    public imageMax: string;
+
     constructor(
-    ) { }
+    ) {
+        /* creo el arreglo con las variables en JSon */
+        this.images = [
+            {
+                min: 'https://s3.amazonaws.com/seller.center.exito.images/imagesDev/products/845/PMK0000006095845/MK00000006095845_xs_a.jpg',
+                max: 'https://s3.amazonaws.com/seller.center.exito.images/imagesDev/products/845/PMK0000006095845/MK00000006095845_lrg_a.jpg'
+            },
+            {
+                min: 'https://s3.amazonaws.com/seller.center.exito.images/imagesDev/products/845/PMK0000006095845/MK00000006095845_sm_c.jpg',
+                max: 'https://s3.amazonaws.com/seller.center.exito.images/imagesDev/products/845/PMK0000006095845/MK00000006095845_lrg_c.jpg'
+            }
+        ];
+
+        /* asigno la 1 imagen grande del arreglo */
+        this.imageMax = this.images[0]['max'];
+    }
+
+
     ngOnInit() {
 
+    }
+
+    /* funcion que cambia el valor de la variable que contiene la url de la imagen grande y recibe como parametro la url de la imagen grande */
+    changeImage(imageUrlMax: string) {
+        this.imageMax = imageUrlMax;
     }
 }
