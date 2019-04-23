352e15197c93ebb1f131e0babbe113ef884db70b
david.betancur <david.betancur@pragma.com.co>
2019-04-23 11:34:27 -0500

    Pruebas unitarias

--- src/app/secure/offers/list/components/detail-offer/detail-offer.spec.ts
+++ src/app/secure/offers/list/components/detail-offer/detail-offer.spec.ts
@@ -40,7 +40,7 @@ const detailOffer = [
         "imageUrl": null
     }]
 
-fdescribe('Detail offer Component', () => {
+describe('Detail offer Component', () => {
 
     const mockStoresService = jasmine.createSpyObj('StoresService', ['getAllStoresFull', 'changeStateSeller']);
     const mockLoadingService = jasmine.createSpyObj('LoadingService', ['viewSpinner', 'closeSpinner']);
