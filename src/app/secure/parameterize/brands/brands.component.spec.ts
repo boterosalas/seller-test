a5e05b7a2ec0ca37dbd97e01541b6f967163c484
israel tarazona <israel.tarazona@pragma.com.co>
2019-05-21 08:09:54 -0500

    quitar el focus de las pruebas unitarias de marcas

--- src/app/secure/parameterize/brands/brands.component.spec.ts
+++ src/app/secure/parameterize/brands/brands.component.spec.ts
@@ -39,7 +39,7 @@ export interface Brands {
 }
 
 
-fdescribe('BrandsComponent', () => {
+describe('BrandsComponent', () => {
     const registerMenu = {
         Functionalities: [{
             NameFunctionality: 'Crear',
