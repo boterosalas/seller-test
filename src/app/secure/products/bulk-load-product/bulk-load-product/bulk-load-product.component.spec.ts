16da35ac6c0bd2d3d95abc329c1c3b62e164cb8c
Jose Fernan <jose.banguera@pragma.com.co>
2019-05-27 14:35:29 -0500

    Unitarias 2

--- src/app/secure/products/bulk-load-product/bulk-load-product/bulk-load-product.component.spec.ts
+++ src/app/secure/products/bulk-load-product/bulk-load-product/bulk-load-product.component.spec.ts
@@ -36,7 +36,7 @@ export const registerRegex = [
     { Identifier: 'eanCombo', Value: '((^([A-Z0-9]{5,16}))+)+([,](([A-Z0-9]{5,16})))*)$', Module: 'productos' },
 ];
 
-fdescribe('BulkLoad Products Component', () => {
+describe('BulkLoad Products Component', () => {
 
     const mockAuthService = {
         profileType$: new BehaviorSubject<any>('Tienda'),
