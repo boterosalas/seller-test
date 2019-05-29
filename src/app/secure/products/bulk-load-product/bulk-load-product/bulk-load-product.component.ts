7b2e90d79dab04ba196d220b6943ed144a5e4101
Cristian Herley Garzón Moreno <cristian.garzon1495@gmail.com>
2019-05-29 14:41:34 -0500

    Implements regex for products module

--- src/app/secure/products/bulk-load-product/bulk-load-product/bulk-load-product.component.ts
+++ src/app/secure/products/bulk-load-product/bulk-load-product/bulk-load-product.component.ts
@@ -113,7 +113,7 @@ export class BulkLoadProductComponent implements OnInit, TreeSelected {
     eanImageProduct: '',
     SkuShippingSizeProduct: '',
     Package: '',
-    descriptionProduct: '',
+    forbiddenScript: '',
     size: '',
     hexColourCodePDPProduct: '',
     limitCharsSixty: '',
@@ -1744,7 +1744,7 @@ export class BulkLoadProductComponent implements OnInit, TreeSelected {
           }
           break;
         case 'formatDescription':
-          if ((inputtxt.match(this.productsRegex.descriptionProduct))) {
+          if ((inputtxt.match(this.productsRegex.forbiddenScript))) {
             valueReturn = true;
           } else {
             valueReturn = false;
