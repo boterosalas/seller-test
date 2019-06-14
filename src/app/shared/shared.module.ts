24a8920f4d1a1ab861c67a2fbe603893084d6a6d
Cristobal Romero <cristobal.romero@sofka.com.co>
2019-06-14 13:46:10 -0500

    Added history component for tocket support

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { MaterialModule } from '@app/material.module';
-import { SearchSellerComponent } from '@app/shared/components/search-seller/search-seller.component';
-import { EventEmitterSeller } from '@app/shared/events/eventEmitter-seller.service';
-import { ControlMessagesComponent } from '@shared/components/control-messages/control-messages.component';
+import { MaterialModule } from "@app/material.module";
+import { SearchSellerComponent } from "@app/shared/components/search-seller/search-seller.component";
+import { EventEmitterSeller } from "@app/shared/events/eventEmitter-seller.service";
+import { ControlMessagesComponent } from "@shared/components/control-messages/control-messages.component";
 
-import { CitiesModule } from './components/cities';
-import { StatesModule } from './components/states';
-import { ToolbarOptionsModule } from './components/toolbar-options';
-import { ToolbarTittleModule } from './components/toolbar-tittle';
-import { CdkDetailRowDirective, NoWhitespaceDirective } from './directives';
-import { ErrorDialogComponent } from './components/dialogs/error-dialog.component';
-import { DialogWithFormComponent } from './components/dialog-with-form/dialog-with-form.component';
-import { CreateProcessDialogComponent } from './components/create-process-dialog/create-process-dialog.component';
-import { RouterModule } from '@angular/router';
-
+import { CitiesModule } from "./components/cities";
+import { StatesModule } from "./components/states";
+import { ToolbarOptionsModule } from "./components/toolbar-options";
+import { ToolbarTittleModule } from "./components/toolbar-tittle";
+import { CdkDetailRowDirective, NoWhitespaceDirective } from "./directives";
+import { ErrorDialogComponent } from "./components/dialogs/error-dialog.component";
+import { DialogWithFormComponent } from "./components/dialog-with-form/dialog-with-form.component";
+import { CreateProcessDialogComponent } from "./components/create-process-dialog/create-process-dialog.component";
+import { RouterModule } from "@angular/router";
+import { HistoryCardComponent } from "./components/history-card/history-card.component";
 
 @NgModule({
   imports: [
@@ -33,7 +33,8 @@ import { RouterModule } from '@angular/router';
     ControlMessagesComponent,
     ErrorDialogComponent,
     DialogWithFormComponent,
-    CreateProcessDialogComponent
+    CreateProcessDialogComponent,
+    HistoryCardComponent
   ],
   exports: [
     MaterialModule,
@@ -54,9 +55,6 @@ import { RouterModule } from '@angular/router';
     DialogWithFormComponent,
     CreateProcessDialogComponent
   ],
-  providers: [
-    EventEmitterSeller,
-  ]
+  providers: [EventEmitterSeller]
 })
-export class SharedModule {
-}
+export class SharedModule {}
