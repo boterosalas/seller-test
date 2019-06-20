6817227560fe243ad0309a887481db9e0d63bc06
Cristian Herley Garzón Moreno <cristian.garzon1495@gmail.com>
2019-06-20 13:55:32 -0500

    Translate service

--- src/app/core/translate/language.service.ts
+++ src/app/core/translate/language.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { distinctUntilChanged, takeUntil, switchMap } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material';
 import { LoadingService } from '../global';
 
@@ -22,21 +22,50 @@ export class LanguageService {
   lenguage$ = new BehaviorSubject(null);
   esData;
   enData;
+  currentLanguage;
+  existFiles$ = new BehaviorSubject(null);
 
   constructor(private http: HttpClient,
     private snackBar: MatSnackBar,
     private loadingService: LoadingService) {
-      console.log('me instancio');
-    this.http.get('./assets/locale/es.json').subscribe(data => {
-      this.esData = data;
-      this.http.get('./assets/locale/en.json').subscribe(dataEn => {
-        this.enData = dataEn;
-        const currentLanguage = this.getLanguageLocalStorage();
-        if (!currentLanguage || this.lenguage$.getValue() !== currentLanguage) {
-          !!this.lenguage$.getValue() ? this.setLanguage(this.lenguage$.getValue()) : this.setLanguage('Es');
-        }
-      });
-    });
+    console.log('me instancio');
+    this.getLanguagesFile();
+  }
+
+  getLanguagesFile(): Promise<any> {
+    return new Promise(() => {
+      this.currentLanguage = this.getLanguageLocalStorage();
+      if (!this.currentLanguage && !this.lenguage$.getValue()) {
+        const lang = navigator.language.toString().toLowerCase().includes('es') ? 'Es' : 'En';
+        this.setLanguage(lang);
+      }
+      const request = this.http.get('./assets/locale/es.json');
+      if (!this.enData && !this.esData) {
+        timer(0, 500).pipe(takeUntil(this.existFiles$), switchMap(() => request)).subscribe((res) => {
+          this.esData = res;
+          this.http.get('./assets/locale/en.json').subscribe(dataEn => {
+            this.enData = dataEn;
+            this.existFiles$.next(true);
+            if (!this.currentLanguage || this.lenguage$.getValue() !== this.currentLanguage) {
+              !!this.lenguage$.getValue() ? this.setLanguage(this.lenguage$.getValue()) : this.setLanguage('Es');
+            }
+            Promise.resolve();
+          });
+        });
+      } else {
+        Promise.resolve();
+      }
+    })
+    // this.http.get('./assets/locale/es.json').subscribe(data => {
+    //   this.esData = data;
+    //   this.http.get('./assets/locale/en.json').subscribe(dataEn => {
+    //     this.enData = dataEn;
+    //     if (!this.currentLanguage || this.lenguage$.getValue() !== this.currentLanguage) {
+    //       console.log('si entro aca');
+    //       !!this.lenguage$.getValue() ? this.setLanguage(this.lenguage$.getValue()) : this.setLanguage('Es');
+    //     }
+    //   });
+    // });
   }
 
   setLanguage(languageId: string) {
@@ -63,8 +92,16 @@ export class LanguageService {
    * @param key Llave para obtener el texto
    */
   getValue(key: string): string {
+    // return this.getValueKey().then(val => {
+
+    // }):
+    return '';
+  }
+
+  getValueKey(key): Promise<string> {
     const properties = key.split('.');
     const val = this.lenguage$.getValue();
+
     try {
       const value = properties.reduce((past, current) => {
         return past[current.trim()];
