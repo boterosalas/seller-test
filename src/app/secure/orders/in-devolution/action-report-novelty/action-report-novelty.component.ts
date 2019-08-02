e411c79dd4e8a1d6b13a9b6935e2af4cde1aed90
Jose Fernan <jose.banguera@pragma.com.co>
2019-08-02 13:53:39 -0500

    PU modulo ordenes

--- src/app/secure/orders/in-devolution/action-report-novelty/action-report-novelty.component.ts
+++ src/app/secure/orders/in-devolution/action-report-novelty/action-report-novelty.component.ts
@@ -94,8 +94,10 @@ export class ActionReportNoveltyComponent implements OnInit {
    *
    * @memberof ActionReportNoveltyComponent
    */
-  reportNovelty(myform) {
+  reportNovelty(myform: any) {
     // busco la razon seleccionada por el usuario
+
+    console.log('myform', myform);
     const reason = this.reasonRejection.find(x => x.idMotivoSolicitudReversion === myform.value.reason);
 
     // Json para realizar el envio
