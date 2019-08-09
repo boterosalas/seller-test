fdaea54e51290826897f6877882a1cb2b336a279
Cristian Herley Garzón Moreno <cristian.garzon1495@gmail.com>
2019-08-09 12:05:54 -0500

    Implements top

--- src/app/secure/offers/stores/tree/components/exception-brand/exception-brand.component.ts
+++ src/app/secure/offers/stores/tree/components/exception-brand/exception-brand.component.ts
@@ -16,61 +16,76 @@ import { LoadingService } from '@app/core';
 export class ExceptionBrandComponent implements OnInit {
 
   form: FormGroup;
+  typeForm: FormGroup;
   @ViewChild('dialogContent') content: TemplateRef<any>;
 
-  displayedColumns = ['Brand', 'Exception', 'options'];
-  displayedColumnsInModal = ['Brand', 'Exception'];
-
+  displayedColumns = ['Brand', 'Comission', 'options'];
+  displayedColumnsInModal = ['Brand', 'Comission'];
   validation = new BehaviorSubject(true);
-
-  brands = [
-    { Name: 'asdlfasdflk' },
-    { Name: 'Hydrogen' },
-    { Name: 'Helium' },
-    { Name: 'Lithium' },
-    { Name: 'Beryllium' },
-    { Name: 'Boron' },
-    { Name: 'Carbon' },
-    { Name: 'Nitrogen' },
-    { Name: 'Oxygen' },
-    { Name: 'Fluorine' },
-    { Name: 'Neon' },
-    { Name: 'Neon 2' },
-    { Name: 'Neon 3' },
-    { Name: 'Neon 4' },
-    { Name: 'Neon 5' },
-    { Name: 'Neon 6' },
-    { Name: 'Neon 7' },
-  ];
-
+  brands = [];
   selectedBrands = [];
   selectedBrandsSources = new MatTableDataSource(this.selectedBrands);
-
   filterBrands = [];
-
   preDataSource = [
-    { Id: 1, Exception: 1, Brand: 'Hydrogen' },
-    { Id: 2, Exception: 2, Brand: 'Helium' },
-    { Id: 3, Exception: 3, Brand: 'Lithium' },
-    { Id: 4, Exception: 4, Brand: 'Beryllium' },
-    { Id: 5, Exception: 5, Brand: 'Boron' },
-    { Id: 6, Exception: 6, Brand: 'Carbon' },
-    { Id: 7, Exception: 7, Brand: 'Nitrogen' },
-    { Id: 8, Exception: 8, Brand: 'Oxygen' },
-    { Id: 9, Exception: 9, Brand: 'Fluorine' },
-    { Id: 10, Exception: 10, Brand: 'Neon' },
+    { Id: 1, Comission: 1, Brand: 'Hydrogen' },
+    { Id: 2, Comission: 2, Brand: 'Helium' },
+    { Id: 3, Comission: 3, Brand: 'Lithium' },
+    { Id: 4, Comission: 4, Brand: 'Beryllium' },
+    { Id: 5, Comission: 5, Brand: 'Boron' },
+    { Id: 6, Comission: 6, Brand: 'Carbon' },
+    { Id: 7, Comission: 7, Brand: 'Nitrogen' },
+    { Id: 8, Comission: 8, Brand: 'Oxygen' },
+    { Id: 9, Comission: 9, Brand: 'Fluorine' },
+    { Id: 10, Comission: 10, Brand: 'Neon' },
   ];
 
   dataSource: MatTableDataSource<any>;
 
   regex;
 
+
+  typeException = ['Marca'];
+
   constructor(private dialog: MatDialog,
     private fb: FormBuilder,
     private regexService: BasicInformationService,
-    private loadingService: LoadingService) {
+    private loadingService: LoadingService,
+    private brandsService: BasicInformationService) {
+      this.typeForm = this.fb.group({
+        type: ['']
+      })
     this.getRegex();
+    this.getBrands();
     this.dataSource = new MatTableDataSource(this.preDataSource);
+    this.typeForm.get('type').valueChanges.subscribe( (val) => {
+      this.changeType(val);
+    });
+  }
+
+  changeType(val: any) {
+
+  }
+
+  getBrands() {
+    this.loadingService.viewSpinner();
+    this.brandsService.getActiveBrands().subscribe(brands => {
+      this.loadingService.closeSpinner();
+      const initialBrands = brands.Data.Brands;
+
+      this.brands = initialBrands.sort((a, b) => {
+        if (a.Name > b.Name) {
+          return 1;
+        }
+        if (a.Name < b.Name) {
+          return -1;
+        }
+        return 0;
+      });
+      this.brands = this.brands.map(element => {
+        const newElement = { Name: element.Name };
+        return newElement;
+      });
+    });
   }
 
   getRegex() {
@@ -93,9 +108,9 @@ export class ExceptionBrandComponent implements OnInit {
     this.form = this.fb.group({
       Id: ['',],
       Brand: ['', Validators.compose([trimField, Validators.required, Validators.minLength(2)])],
-      Exception: ['', Validators.compose([trimField, Validators.required, Validators.max(30), Validators.min(0), Validators.pattern(this.regex)])]
+      Comission: ['', Validators.compose([trimField, Validators.required, Validators.max(30), Validators.min(0), Validators.pattern(this.regex)])]
     });
-    this.Exception.disable();
+    this.Comission.disable();
     this.Brand.valueChanges.pipe(distinctUntilChanged(), debounceTime(300)).subscribe(val => {
       if (!!val && val.length >= 2) {
         this.filterBrands = this.brands.filter(brand => brand.Name.toString().toLowerCase().includes(val.toLowerCase()));
@@ -104,12 +119,12 @@ export class ExceptionBrandComponent implements OnInit {
           this.Brand.setErrors({ pattern: true });
         } else {
           this.Brand.setErrors(null);
-          this.Exception.enable();
+          this.Comission.enable();
         }
       } else if (!val) {
         this.filterBrands = [];
         this.Brand.setErrors({ required: true });
-        this.Exception.disable();
+        this.Comission.disable();
       } else {
         this.Brand.setErrors({ pattern: true });
       }
@@ -120,26 +135,52 @@ export class ExceptionBrandComponent implements OnInit {
   ngOnInit() {
   }
 
-  openDialog(element?: any) {
+  openDialog(action: string, element?: any) {
     this.form.setValidators(null);
-    const data = !!element ? this.putDataForUpdate(element) : this.putDataForCreate();
+    const data = !!(action === 'edit') ? this.putDataForUpdate(element) : !!(action === 'create') ? this.putDataForCreate() : this.putDataForDelete();
     const dialogRef = this.dialog.open(DialogWithFormComponent, {
       data: data,
       width: '55%',
       minWidth: '280px'
     });
-    this.configDialog(dialogRef.componentInstance);
+    const dataToConfig = !!element ? { action, element } : { action };
+    this.configDialog(dialogRef.componentInstance, dataToConfig);
     dialogRef.afterClosed().subscribe(() => {
       this.selectedBrands = [];
       this.validation.next(true);
     });
   }
 
-  configDialog(dialog: any) {
-    dialog.content = this.content;
+  configDialog(dialog: any, data: any) {
+    if (data.action !== 'delete'){
+      dialog.content = this.content;
+    }
     dialog.confirmation = () => {
-
+      switch (data.action) {
+        case 'create':
+          this.selectedBrands.forEach(element => {
+            this.preDataSource.push(element);
+          });
+          this.dataSource.data = this.preDataSource;
+          break;
+        case 'edit':
+          break;
+        case 'delete':
+          this.removeElement(data.element);
+          break;
+      };
     };
+  }
+
+  putDataForDelete() {
+    const form = null;
+    const title = 'Eliminar excepción';
+    const message = 'Estás seguro que deseas eliminar esta excepción';
+    const messageCenter = true;
+    const showButtons = true;
+    const icon = null;
+    const btnConfirmationText = null;
+    return { form, title, message, messageCenter, showButtons, icon, btnConfirmationText };
   }
 
   putDataForUpdate(element: any) {
@@ -169,7 +210,6 @@ export class ExceptionBrandComponent implements OnInit {
   }
 
   addBrand() {
-    console.log(this.form.value);
     this.selectedBrands.push(Object.assign(this.form.value, {}));
     this.selectedBrandsSources.data = this.selectedBrands;
     this.form.reset();
@@ -177,14 +217,20 @@ export class ExceptionBrandComponent implements OnInit {
   }
 
   deleteElement(element: any) {
-    console.log('remove: ', element);
+    this.openDialog('delete', element);
+  }
+
+  removeElement(element) {
+    const index = this.preDataSource.findIndex(value => value === element);
+    !!index && this.preDataSource.splice(index, 1);
+    this.dataSource.data = this.preDataSource;
   }
 
   get Brand(): FormControl {
     return this.form.get('Brand') as FormControl;
   }
 
-  get Exception(): FormControl {
-    return this.form.get('Exception') as FormControl;
+  get Comission(): FormControl {
+    return this.form.get('Comission') as FormControl;
   }
 }
---
+++ src/app/secure/offers/stores/tree/components/exception-brand/exception-brand.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ExceptionBrandService } from './exception-brand.service';
+
+describe('ExceptionBrandService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ExceptionBrandService = TestBed.get(ExceptionBrandService);
+    expect(service).toBeTruthy();
+  });
+});
