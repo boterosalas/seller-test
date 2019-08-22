95be330787bfbd4be1f096a2a32421c4d0dcf292
Jose Fernan <jose.banguera@pragma.com.co>
2019-08-22 16:58:57 -0500

    PU reporte de ofertas crea componente

--- src/app/secure/reports/report-offert/report-offert.component.spec.ts
+++ src/app/secure/reports/report-offert/report-offert.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ReportOffertComponent } from './report-offert.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '@app/material.module';
 
 describe('ReportOffertComponent', () => {
   let component: ReportOffertComponent;
@@ -8,9 +13,19 @@ describe('ReportOffertComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReportOffertComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MaterialModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        HttpClientModule
+      ],
+      declarations: [ ReportOffertComponent ],
+      providers: [
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
