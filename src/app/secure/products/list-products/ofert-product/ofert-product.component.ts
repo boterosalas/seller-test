790a92eb2ac122f6d984c1ee33220c85cd64b28e
Jose Fernan <jose.banguera@pragma.com.co>
2019-09-09 17:08:46 -0500

    Se habilita tipo de moneda en listado de ofertas y productos para prueba de MPI

--- src/app/secure/products/list-products/ofert-product/ofert-product.component.ts
+++ src/app/secure/products/list-products/ofert-product/ofert-product.component.ts
@@ -109,7 +109,7 @@ export class OfertExpandedProductComponent implements OnInit {
         });
 
         // Borrar esta linea, para Internacional
-        this.ofertProduct.get('Currency').disable();
+        // this.ofertProduct.get('Currency').disable();
 
         this.matcher = new MyErrorStateMatcher();
         // tslint:disable-next-line:no-shadowed-variable
