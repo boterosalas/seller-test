fec05d9fe31aaef6665a0afa365590361d799a81
pragmaExito <israel.tarazona@pragma.com.co>
2019-09-24 14:52:45 -0500

    no message

--- src/app/core/http/auth.interceptor.ts
+++ src/app/core/http/auth.interceptor.ts
@@ -18,18 +18,34 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    if(!req.url.toString().includes('assets')) {
+    if (!req.url.toString().includes('assets')) {
       const idToken = this.cognitoUtil.getTokenLocalStorage();
       const headers = new HttpHeaders({ 'Authorization': idToken, 'Content-type': 'application/json; charset=utf-8' });
-
+      const culture = this.addCulture(req);
       // Clona la solicitud y reemplaza los encabezados originales con
       // encabezados actualizados con la autorización.
+      // colocar la variable de url modificada para la cultura (idioma)
+      // console.log(culture);
+      // console.log(req.url);
       const authReq = req.clone({
         headers: headers
       });
-
       return next.handle(authReq);
     }
     return next.handle(req);
+  }
+
+  addCulture(req: HttpRequest<any>) {
+    if (req && req.url) {
+      const validate = req.url.includes('?') || req.url.includes('&') ;
+        if (validate) {
+          const culture = '&culture=es-CO';
+          return  culture;
+        } else {
+          const culture = '/?culture=es-CO';
+          return  culture;
+        }
+
+    }
   }
 }
