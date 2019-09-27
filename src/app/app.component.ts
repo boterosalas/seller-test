028bb4c70742f2b5e99dad4d828c26df83ac32fc
Jose Fernan <jose.banguera@pragma.com.co>
2019-09-27 09:43:28 -0500

    Flags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { environment } from '@env/environment';
 import { TranslateService } from '@ngx-translate/core';
 
 const log = new Logger('AppComponent');
-export const langs = ['EN', 'ES'];
+export const langs = ['US', 'ES'];
 
 @Component({
   selector: 'app-root',
