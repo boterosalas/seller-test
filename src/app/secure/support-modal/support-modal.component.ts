b1575cc07e75c2f159d6a675aef5f2b9ecbb3236
Cristobal Romero <cristobal.romero@sofka.com.co>
2019-11-25 14:13:45 -0500

    Added new fields in creaction of case

--- src/app/secure/support-modal/support-modal.component.ts
+++ src/app/secure/support-modal/support-modal.component.ts
@@ -1,44 +1,45 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { MatDialogRef } from '@angular/material';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { MatDialogRef } from "@angular/material";
 
-import { UserParametersService } from '@app/core/aws-cognito';
-import { Logger } from '@app/core/util/logger.service';
-import { ComponentsService } from '@shared/services/components.service';
+import { UserParametersService } from "@app/core/aws-cognito";
+import { Logger } from "@app/core/util/logger.service";
+import { ComponentsService } from "@shared/services/components.service";
 
-import { SupportService } from './support.service';
-import { UserInformation } from '@app/shared';
-import { LoadingService } from '@app/core/global/loading/loading.service';
-import { BasicInformationService } from '../products/create-product-unit/basic-information/basic-information.component.service';
-import { TranslateService } from '@ngx-translate/core';
-import { from } from 'rxjs';
-import { map, toArray } from 'rxjs/operators';
-import { File } from '@app/shared/components/upload-button/configuration.model';
-import { ACCEPT_TYPE } from '@app/shared/models';
+import { SupportService } from "./support.service";
+import { UserInformation } from "@app/shared";
+import { LoadingService } from "@app/core/global/loading/loading.service";
+import { BasicInformationService } from "../products/create-product-unit/basic-information/basic-information.component.service";
+import { TranslateService } from "@ngx-translate/core";
+import { from } from "rxjs";
+import { map, toArray } from "rxjs/operators";
+import { File } from "@app/shared/components/upload-button/configuration.model";
+import { ACCEPT_TYPE } from "@app/shared/models";
 
 // log component
-const log = new Logger('SupportModalComponent');
+const log = new Logger("SupportModalComponent");
 
 /**
  * Component que permite desplegar un modal donde se solicitaran unos datos para realizar el envió del mensaje de soporte
  */
 @Component({
-  selector: 'app-support-modal',
-  templateUrl: './support-modal.component.html',
-  styleUrls: ['./support-modal.component.scss'],
-  providers: [
-    SupportService,
-    ComponentsService
-  ]
+  selector: "app-support-modal",
+  templateUrl: "./support-modal.component.html",
+  styleUrls: ["./support-modal.component.scss"],
+  providers: [SupportService, ComponentsService]
 })
 
 /**
  * SupportModalComponent
  */
 export class SupportModalComponent implements OnInit {
-
   // Input file de la vista
-  @ViewChild('fileInput') fileInput: ElementRef;
+  @ViewChild("fileInput") fileInput: ElementRef;
   //  Formulario para realizar la busqueda
   myform: FormGroup;
   // user info
@@ -72,8 +73,8 @@ export class SupportModalComponent implements OnInit {
     public SUPPORT: SupportService,
     public userParams: UserParametersService,
     public loadingService: LoadingService,
-    private languageService: TranslateService,
-  ) { }
+    private languageService: TranslateService
+  ) {}
 
   /**
    * @memberof SupportModalComponent
@@ -97,22 +98,45 @@ export class SupportModalComponent implements OnInit {
     this.dialogRef.close(false);
   }
 
-
   /**
    * Método para crear el formulario
    * @memberof SupportModalComponent
    */
   createForm(user: any) {
     this.myform = new FormGroup({
-      nit: new FormControl(user.sellerNit, Validators.compose([Validators.required])),
-      caseMarketplaceName: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.instant('nameCaseOrders'))])),
-      account: new FormControl(user.sellerName, Validators.compose([Validators.required])),
-      emailContact: new FormControl(user.sellerEmail, Validators.compose([Validators.required, Validators.email])),
-      typeOfRequirement: new FormControl('', Validators.compose([Validators.required])),
-      description: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.instant('descriptionOrders'))])),
-      contact: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.instant('contactOrders'))])),
-      reason: new FormControl('', Validators.compose([Validators.required])),
-      subCategory: new FormControl('', Validators.compose([Validators.required])),
+      nit: new FormControl(
+        user.sellerNit,
+        Validators.compose([Validators.required])
+      ),
+      account: new FormControl(
+        user.sellerName,
+        Validators.compose([Validators.required])
+      ),
+      emailContact: new FormControl(
+        user.sellerEmail,
+        Validators.compose([Validators.required, Validators.email])
+      ),
+      typeOfRequirement: new FormControl(
+        "",
+        Validators.compose([Validators.required])
+      ),
+      description: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.instant("descriptionOrders"))
+        ])
+      ),
+      contact: new FormControl(
+        user.sellerName,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.instant("contactOrders"))
+        ])
+      ),
+      reason: new FormControl("", Validators.compose([Validators.required])),
+      subCategory: new FormControl(""),
+      category: new FormControl("")
     });
   }
 
@@ -122,12 +146,11 @@ export class SupportModalComponent implements OnInit {
    * @memberof SupportModalComponent
    */
   public valdiateFormSupport(): void {
-    const param = ['orders', null];
+    const param = ["orders", null];
     this.SUPPORT.getRegexFormSupport(param).subscribe(res => {
       this.validateRegex = JSON.parse(res.body.body);
       this.createForm(this.user);
     });
-
   }
 
   public instant(name: string): string {
@@ -149,29 +172,44 @@ export class SupportModalComponent implements OnInit {
   sendSupportMessage(form: any) {
     // Envió el mensaje de soporte. luego de retornar el servicio correctamente,
     // me pasan el id del soporte para asociar el archivo adjunto a la orden y poder realizar el envió
+    console.log(form.value);
     const messageSupport = {
       contact: form.value.contact.trim(),
       description: form.value.description.trim(),
       emailContact: form.value.emailContact,
-      caseMarketplaceName: form.value.caseMarketplaceName,
       account: this.user.sellerName,
       nit: this.user.sellerNit,
       reason: form.value.reason,
       typeOfRequirement: form.value.typeOfRequirement.trim(),
-      caseOrigin: 'Sitio web marketplace',
-      caseMarketplaceOwner: 'Soporte MarketPlace',
-      attachedFile: this.response.attachments,
+      caseOrigin: "Sitio web marketplace",
+      caseMarketplaceOwner: "Soporte MarketPlace",
+      attachedFile: this.response.attachments
     };
     this.loadingService.viewSpinner();
-    this.SUPPORT.sendSupportMessage(this.user['access_token'], messageSupport).subscribe((res: any) => {
-      this.loadingService.closeSpinner();
-      this.COMPONENT.openSnackBar(this.languageService.instant('secure.support_modal.ts_send_msj'), this.languageService.instant('actions.accpet_min'), 10000);
-      debugger
-      this.onNoClick();
-    }, err => {
-      this.loadingService.closeSpinner();
-      this.COMPONENT.openSnackBar(this.languageService.instant('secure.support_modal.ts_error_msj_support'), this.languageService.instant('actions.accpet_min'), 10000);
-    });
+    this.SUPPORT.sendSupportMessage(
+      this.user["access_token"],
+      messageSupport
+    ).subscribe(
+      (res: any) => {
+        this.loadingService.closeSpinner();
+        this.COMPONENT.openSnackBar(
+          this.languageService.instant("secure.support_modal.ts_send_msj"),
+          this.languageService.instant("actions.accpet_min"),
+          10000
+        );
+        this.onNoClick();
+      },
+      err => {
+        this.loadingService.closeSpinner();
+        this.COMPONENT.openSnackBar(
+          this.languageService.instant(
+            "secure.support_modal.ts_error_msj_support"
+          ),
+          this.languageService.instant("actions.accpet_min"),
+          10000
+        );
+      }
+    );
   }
 
   /**
