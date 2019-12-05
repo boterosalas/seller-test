5160b0fd32e82f31589317c8e57d876294af6ec4
Manuel Julian Charris Blanco <manuel.charris@sofka.com.co>
2019-12-05 15:16:03 -0500

    remove debugger and logs

--- src/app/secure/support-modal/support-modal.component.ts
+++ src/app/secure/support-modal/support-modal.component.ts
@@ -172,7 +172,7 @@ export class SupportModalComponent implements OnInit {
     this.scReasonTypes = [];
     this.classificationSelected = item;
     this.scRequiered = item.fields;
-    debugger
+
     this.groupByKey(this.omsCategories, 'classification',
       {
         classification: item.classification
@@ -210,15 +210,12 @@ export class SupportModalComponent implements OnInit {
         subcategories.forEach(e => {
           if (this.classificationSelected.classification === e.classification) {
             this.scSubcategories.push(e);
-            debugger
           }
         });
       });
     if (!item.subcategory) {
       this.scReasonTypes = item.type;
     }
-    console.log(this.scSubcategories);
-    debugger
   }
 
   onClickSubcategoryOption(item: CaseCategory) {
