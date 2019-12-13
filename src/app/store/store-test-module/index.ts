2682e89abde7430603ce30a1397a54608868e0a1
Manuel Julian Charris Blanco <manuel.charris@sofka.com.co>
2019-12-13 08:38:39 -0500

    comentar lineas innecesarias

--- src/app/store/store-test-module/index.ts
+++ src/app/store/store-test-module/index.ts
@@ -1,7 +1,7 @@
 // import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 import { NgModule } from '@angular/core';
 import { CoreStoreModule } from '../';
-import { SellerSupportMockService } from '@app/secure/seller-support-center/services/test-mocks/seller-support-mock.service';
+//import { SellerSupportMockService } from '@app/secure/seller-support-center/services/test-mocks/seller-support-mock.service';
 import { SellerSupportCenterService } from '@app/secure/seller-support-center/services/seller-support-center.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -9,7 +9,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 @NgModule({
   imports: [CoreStoreModule],
   providers: [
-    { provide: SellerSupportCenterService, useClass: SellerSupportMockService },
+    //{ provide: SellerSupportCenterService, useClass: SellerSupportMockService },
     { provide: HttpClient, useClass: HttpClientTestingModule }
   ]
 })
