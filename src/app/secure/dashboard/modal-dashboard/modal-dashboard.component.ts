dfb670da883649c4887b7daa29c3b7f381b61620
pragmaExito <israel.tarazona@pragma.com.co>
2020-03-05 11:01:15 -0500

    no message

--- src/app/secure/dashboard/modal-dashboard/modal-dashboard.component.ts
+++ src/app/secure/dashboard/modal-dashboard/modal-dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { DialogData } from '@app/secure/support-modal/support-modal.component';
 
 @Component({
   selector: 'app-modal-dashboard',
@@ -11,7 +10,7 @@ export class ModalDashboardComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<ModalDashboardComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
   ngOnInit() {
