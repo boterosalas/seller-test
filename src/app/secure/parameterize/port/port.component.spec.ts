b7ff4df5799bffb5f8d59fb131eb0a28ecf1fcf3
pragmaExito <israel.tarazona@pragma.com.co>
2020-03-16 16:42:55 -0500

    no message

--- src/app/secure/parameterize/port/port.component.spec.ts
+++ src/app/secure/parameterize/port/port.component.spec.ts
@@ -25,39 +25,39 @@ export const registerRegex = [
   { Identifier: 'formatIntegerNumber', Value: '^[0-9]+([.][0-9]{2})?$', Module: 'parametrizacion' },
 ];
 
-fdescribe('PortComponent', () => {
+describe('PortComponent', () => {
 
   const registerMenu = {
     Functionalities: [{
-        NameFunctionality: 'Crear',
-        ShowFunctionality: true,
-        nameFunctionalityBack: 'Crear'
+      NameFunctionality: 'Crear',
+      ShowFunctionality: true,
+      nameFunctionalityBack: 'Crear'
     }],
-};
+  };
 
-const responseEmpty = {
-  body: '{"Message":"Operación realizada éxitosamente.","Errors":[],"Data":null}',
-  headers: null,
-  isBase64Encoded: false,
-  multiValueHeaders: null,
-  statusCode: 200
-};
-const response = {
-  body: '{"Message":"Operación realizada éxitosamente.","Errors":[],"Data":[{},{},{}]}',
-  headers: null,
-  isBase64Encoded: false,
-  multiValueHeaders: null,
-  statusCode: 200
-};
+  const responseEmpty = {
+    body: '{"Message":"Operación realizada éxitosamente.","Errors":[],"Data":null}',
+    headers: null,
+    isBase64Encoded: false,
+    multiValueHeaders: null,
+    statusCode: 200
+  };
+  const response = {
+    body: '{"Message":"Operación realizada éxitosamente.","Errors":[],"Data":[{},{},{}]}',
+    headers: null,
+    isBase64Encoded: false,
+    multiValueHeaders: null,
+    statusCode: 200
+  };
 
-const data = {
-  title: '',
-  message: '',
-  icon: '',
-  form: null,
-  btnConfirmationText: null,
-  showButtons: false
-};
+  const data = {
+    title: '',
+    message: '',
+    icon: '',
+    form: null,
+    btnConfirmationText: null,
+    showButtons: false
+  };
 
 
 
@@ -84,71 +84,71 @@ const data = {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-        imports: [
-            MaterialModule,
-            ReactiveFormsModule,
-            FormsModule,
-            RouterTestingModule,
-            BrowserAnimationsModule,
-            HttpClientModule,
-            SharedModule,
-            MatSnackBarModule
-        ],
-        declarations: [
-          PortComponent
-        ],
-        providers: [
-            { provide: PortCollectionService, useValue: mockPortCollectionService },
-            EndpointService,
-            { provide: LoadingService, useValue: mockLoadingService },
-            SupportService,
-            { provide: AuthService, useValue: mockAuthService },
-            UserParametersService,
-            CognitoUtil,
-            { provide: UserLoginService, useValue: mockUserLoginService },
-            { provide: ModalService, useValue: mockDialogError },
-            { provide: MatDialog, useValue: mockDialog },
-            { provide: MAT_DIALOG_DATA, useValue: data },
-            { provide: MatDialogRef, useValue: mockDialogRef },
-            ShellComponent,
-            ComponentsService,
-            EventEmitterOrders,
-            StoresService
-        ],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+      imports: [
+        MaterialModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        SharedModule,
+        MatSnackBarModule
+      ],
+      declarations: [
+        PortComponent
+      ],
+      providers: [
+        { provide: PortCollectionService, useValue: mockPortCollectionService },
+        EndpointService,
+        { provide: LoadingService, useValue: mockLoadingService },
+        SupportService,
+        { provide: AuthService, useValue: mockAuthService },
+        UserParametersService,
+        CognitoUtil,
+        { provide: UserLoginService, useValue: mockUserLoginService },
+        { provide: ModalService, useValue: mockDialogError },
+        { provide: MatDialog, useValue: mockDialog },
+        { provide: MAT_DIALOG_DATA, useValue: data },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        ShellComponent,
+        ComponentsService,
+        EventEmitterOrders,
+        StoresService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
 
     TestBed.overrideModule(BrowserDynamicTestingModule, {
-        set: {
-            entryComponents: [DialogWithFormComponent]
-        }
+      set: {
+        entryComponents: [DialogWithFormComponent]
+      }
     });
-}));
+  }));
 
-beforeEach(() => {
-  mockAuthService.getMenu.and.returnValue(registerMenu);
-  fixture = TestBed.createComponent(PortComponent);
-  portComponent = fixture.componentInstance;
-  portCollectionService = TestBed.get(PortCollectionService);
-  supportService = TestBed.get(SupportService);
-  dialogFixture = TestBed.createComponent(DialogWithFormComponent);
-  dialogComponent = dialogFixture.componentInstance;
-  mockSupportService.getRegexFormSupport.and.returnValue(of(registerRegex));
-  mockPortCollectionService.getAllPort.and.returnValue(of(response));
-  portComponent.keywords = ['PARIS', 'MEXICO', 'ESPAÑA'];
-  fixture.detectChanges();
-});
+  beforeEach(() => {
+    mockAuthService.getMenu.and.returnValue(registerMenu);
+    fixture = TestBed.createComponent(PortComponent);
+    portComponent = fixture.componentInstance;
+    portCollectionService = TestBed.get(PortCollectionService);
+    supportService = TestBed.get(SupportService);
+    dialogFixture = TestBed.createComponent(DialogWithFormComponent);
+    dialogComponent = dialogFixture.componentInstance;
+    mockSupportService.getRegexFormSupport.and.returnValue(of(registerRegex));
+    mockPortCollectionService.getAllPort.and.returnValue(of(response));
+    portComponent.keywords = ['PARIS', 'MEXICO', 'ESPAÑA'];
+    fixture.detectChanges();
+  });
 
 
-  it('should create', function(done: any){
+  it('should create', function (done: any) {
     expect(portComponent).toBeTruthy();
     done();
   });
 
-  it('toggle Filter', function (){
+  it('toggle Filter', function () {
     portComponent.toggleFilterPorts();
   });
-  it('Delete key', function (){
+  it('Delete key', function () {
     portComponent.deleteKeywork(1);
   });
 
@@ -160,9 +160,82 @@ beforeEach(() => {
 
     it('Edit offert', () => {
       portComponent.deleteKeywork(1);
-        // detailOfferComponent.editOffer();
-        // expect(detailOfferComponent.isUpdateOffer).toBeTruthy();
+    });
+    it('Edit offert', () => {
+      portComponent.resetFormModal();
+    });
+    it('on no click', () => {
+      portComponent.onNoClick();
+    });
+    it('clear form', () => {
+      portComponent.clearFormFilter();
+    });
+  });
+  describe('inputs edit', () => {
+    beforeEach(() => {
+      mockPortCollectionService.savePort.and.returnValue(of(true));
+      portComponent.formPort = new FormGroup({
+        country: new FormControl(''),
+        applyCountry: new FormControl(''),
+        address: new FormControl(''),
+        phone: new FormControl(''),
+        insurance_freight: new FormControl(''),
+        preparation: new FormControl(''),
+        shippingCost: new FormControl(''),
+        national_transportation: new FormControl(''),
+        insurance_CIF: new FormControl(''),
+        tariffByKg: new FormControl(''),
+        tariff: new FormControl(''),
+        iva: new FormControl(''),
+      });
     });
 
-});
+    it('save Port', () => {
+      portComponent.savePort();
+    });
+  });
+  describe('set Edit', () => {
+    beforeEach(() => {
+      mockPortCollectionService.savePort.and.returnValue(of(true));
+      portComponent.formPort = new FormGroup({
+        country: new FormControl(''),
+        applyCountry: new FormControl('CHINA'),
+        address: new FormControl(''),
+        phone: new FormControl(''),
+        insurance_freight: new FormControl(''),
+        preparation: new FormControl(''),
+        shippingCost: new FormControl(''),
+        national_transportation: new FormControl(''),
+        insurance_CIF: new FormControl(''),
+        tariffByKg: new FormControl(''),
+        tariff: new FormControl(''),
+        iva: new FormControl(''),
+      });
+      portComponent.data = {
+        data: {
+          id: '',
+          country: '',
+          address: '',
+          phone: '',
+          insurance_freight: '',
+          preparation: '',
+          shippingCost: '',
+          national_transportation: '',
+          insurance_CIF: '',
+          tariffByKg: '',
+          tariff: '',
+          iva: '',
+          countrys: ['MEXICO', 'PANAMA']
+        },
+        typeModal : 2
+      };
+    });
+
+    it('save Port', () => {
+      portComponent.setEdit();
+    });
+    it('save Keyword', () => {
+      portComponent.saveKeyword();
+    });
+  });
 });
