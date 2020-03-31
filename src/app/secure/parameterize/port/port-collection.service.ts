5c0755e3c24cad7994bae4937779d9508f97907f
pragmaExito <israel.tarazona@pragma.com.co>
2020-03-31 08:28:54 -0500

    no message

--- src/app/secure/parameterize/port/port-collection.service.ts
+++ src/app/secure/parameterize/port/port-collection.service.ts
@@ -8,42 +8,49 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PortCollectionService {
 
- data = [
-   {
-     id : 1,
-     name : 'puerto_1',
-     country : 'ESTADOS UNIDOS',
-     address : 'direccion 2',
-     phone: 3053659647,
-     tariff: 0,
-     shippingCost: 0,
-     negotiatedShippingCostPercent: 0,
-     apply: true,
-     countrys : ['ESTADOS UNIDOS', 'ARGENTINA', 'MEXICO', 'FRANCIA', 'BRASIL', 'VENEZUELA', 'COLOMBIA', 'PERU']
-   }
-  ];
 
   constructor(
     private http: HttpClient,
     private api: EndpointService
   ) { }
 
-
+  /**
+   * funcion para obtener todos los puertos registrados
+   *
+   * @param {*} params
+   * @returns
+   * @memberof PortCollectionService
+   */
   getAllPort(params: any) {
     return this.http.get(`${this.api.get('managePort')}/GetDispatchPortsAll`, { observe: 'response' });
   }
-
-  changeApplyCountry(params: any) {
-    return new Observable(observer => {
-      observer.next(!params);
-    });
-  }
+  /**
+   * funcion para salvar un puerto registrado
+   *
+   * @param {*} params
+   * @returns
+   * @memberof PortCollectionService
+   */
   savePort(params: any) {
     return this.http.post(`${this.api.get('managePort')}/RegisterDispatchPort`, params);
   }
+  /**
+   * funcion para actualizar un puerto seleccionado
+   *
+   * @param {*} params
+   * @returns
+   * @memberof PortCollectionService
+   */
   upsertPort(params: any) {
     return this.http.patch(`${this.api.get('managePort')}/UpdateDispatchPort`, params, { observe: 'response' });
   }
+  /**
+   * funcion para filtrar un puerto por pais
+   *
+   * @param {*} params
+   * @returns {Observable<any>}
+   * @memberof PortCollectionService
+   */
   filterPortByCountryName(params: any): Observable<any> {
     return this.http.get(this.api.get('getPortsByCountryName', [params]), { observe: 'response' });
   }
