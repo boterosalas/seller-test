6bf239489dd155b729cf7d1c4c39f3c6e494d9e5
Jose Fernan <jose.banguera@pragma.com.co>
2020-04-20 12:03:24 -0500

    Bug hide header response

--- src/app/shared/components/drop-down-list-header/drop-down-list-header.component.ts
+++ src/app/shared/components/drop-down-list-header/drop-down-list-header.component.ts
@@ -1,13 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { MyProfileService } from '@app/secure/aws-cognito/profile/myprofile.service';
+import { LoadingService } from '@app/core';
 
 @Component({
   selector: 'app-drop-down-list-header',
   templateUrl: './drop-down-list-header.component.html',
   styleUrls: ['./drop-down-list-header.component.scss']
 })
-export class DropDownListHeaderComponent {
+export class DropDownListHeaderComponent implements OnInit {
   @Input() configurations: Array<Configuration>;
+  // Variable para determinar que tipo de perfil esta logueado y determinar los headers
+  hideHeader: any;
+  // Copia del array del listado de headers
+  copyConfigurations: Configuration[];
+
+  constructor(
+  ) {
+    if (localStorage.getItem('typeProfile') === 'administrator') {
+      this.hideHeader = false;
+    } else {
+      this.hideHeader = true;
+    }
+  }
+
+  ngOnInit() {
+    this.showHeader();
+  }
+
+  showHeader() {
+    if (this.hideHeader) {
+      this.configurations = this.configurations;
+    } else {
+      this.copyConfigurations = [...this.configurations];
+      this.copyConfigurations.splice(5, 1);
+    }
+  }
 }
+
 
 export interface Configuration {
   name: string;
