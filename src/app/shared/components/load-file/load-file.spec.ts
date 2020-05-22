64fa7cd0dbd24a329aee67031f7a00a494082969
pragmaExito <israel.tarazona@pragma.com.co>
2020-05-22 17:01:24 -0500

    no message

--- src/app/shared/components/load-file/load-file.spec.ts
+++ src/app/shared/components/load-file/load-file.spec.ts
@@ -1,64 +1,64 @@
-// import { async, TestBed, ComponentFixture, fakeAsync } from '@angular/core/testing';
-// import { LoadFileComponent } from './load-file';
-// import { HttpClient } from '@angular/common/http';
-// import { MAT_DIALOG_DATA, MatDialogRef, MatSnackBar } from '@angular/material';
-// import { MaterialModule } from '@app/material.module';
-// import { ngfModule } from 'angular-file';
-// import { CommonService } from '@app/shared/services/common.service';
-// import { TranslateModule } from '@ngx-translate/core';
+import { async, TestBed, ComponentFixture, fakeAsync } from '@angular/core/testing';
+import { LoadFileComponent } from './load-file';
+import { HttpClient } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MatDialogRef, MatSnackBar } from '@angular/material';
+import { MaterialModule } from '@app/material.module';
+import { ngfModule } from 'angular-file';
+import { CommonService } from '@app/shared/services/common.service';
+import { TranslateModule } from '@ngx-translate/core';
 
-// describe('Probando componente para subir archivos.', () => {
+describe('Probando componente para subir archivos.', () => {
 
-//     let fixture: ComponentFixture<LoadFileComponent>;
-//     let component: LoadFileComponent;
+    let fixture: ComponentFixture<LoadFileComponent>;
+    let component: LoadFileComponent;
 
-//     let httpClient: HttpClient;
-//     httpClient = <HttpClient>{};
+    let httpClient: HttpClient;
+    httpClient = <HttpClient>{};
 
-//     beforeEach(fakeAsync( () => {
-//         TestBed.configureTestingModule({
-//             declarations: [
-//                 LoadFileComponent
-//             ],
-//             providers: [
-//                 { provide: HttpClient, useValue: httpClient },
-//                 { provide: MAT_DIALOG_DATA, useValue: {} },
-//                 { provide: MatDialogRef, useValue: {} },
-//                 { provide: MatDialogRef, useValue: {} },
-//                 { provide: CommonService, useValue: {} },
-//                 { provide: MatSnackBar, useValue: {} },
-//             ],
-//             imports: [
-//                 MaterialModule,
-//                 ngfModule,
-//                 TranslateModule.forRoot({})
+    beforeEach(fakeAsync( () => {
+        TestBed.configureTestingModule({
+            declarations: [
+                LoadFileComponent
+            ],
+            providers: [
+                { provide: HttpClient, useValue: httpClient },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MatDialogRef, useValue: {} },
+                { provide: CommonService, useValue: {} },
+                { provide: MatSnackBar, useValue: {} },
+            ],
+            imports: [
+                MaterialModule,
+                ngfModule,
+                TranslateModule.forRoot({})
 
-//             ]
-//         }).compileComponents();
-//     }));
+            ]
+        }).compileComponents();
+    }));
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(LoadFileComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoadFileComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//     it('Deberia crear LoadFileComponent', () => {
-//         expect(component).toBeTruthy();
-//     });
+    it('Deberia crear LoadFileComponent', () => {
+        expect(component).toBeTruthy();
+    });
 
-//     it('Deberia cancelar la subida del documento', () => {
-//         component.cancel();
-//         expect(component.progress).toBe(0);
-//     });
+    it('Deberia cancelar la subida del documento', () => {
+        component.cancel();
+        expect(component.progress).toBe(0);
+    });
 
-//     it('Deberia intentar guardar el archivo pero no hay archivo', () => {
-//         component.saveFile();
-//         expect(component.lastInvalids).toBeUndefined();
-//     });
+    it('Deberia intentar guardar el archivo pero no hay archivo', () => {
+        component.saveFile();
+        expect(component.lastInvalids).toBeUndefined();
+    });
 
-//     // it('Deberia obtener la fecha actual', () => {
-//     //     expect(component.getDate().getTime()).toBe(new Date().getTime());
-//     // });
+    // it('Deberia obtener la fecha actual', () => {
+    //     expect(component.getDate().getTime()).toBe(new Date().getTime());
+    // });
 
-// });
+});
