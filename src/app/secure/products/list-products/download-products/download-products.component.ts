3a941e784b052e0cede5f1fba8b148cdf434a9ff
Jose Fernan <jose.banguera@pragma.com.co>
2020-06-30 21:10:38 -0500

    Open modal correo electronico

--- src/app/secure/products/list-products/download-products/download-products.component.ts
+++ src/app/secure/products/list-products/download-products/download-products.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { UserInformation } from '@app/shared';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserParametersService } from '@app/core';
 
 export interface DialogData {
   data: any;
@@ -13,12 +16,38 @@ export interface DialogData {
 })
 export class DownloadProductsComponent implements OnInit {
 
+  public user: UserInformation;
+  myform: FormGroup;
+
   constructor(
+    public userParams: UserParametersService,
+    private fb: FormBuilder,
     public dialogRef: MatDialogRef<DownloadProductsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) { }
 
   ngOnInit() {
+    this.getDataUser().then(data => {
+      this.createForm();
+    });
+  }
+
+  async getDataUser() {
+    this.user = await this.userParams.getUserData();
+  }
+
+  createForm() {
+    const email = this.user.sellerEmail;
+    this.myform = this.fb.group({
+      'email': [{ value: email, disabled: false }, Validators.compose([Validators.required, Validators.email])],
+    });
+  }
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+
+  onNoClickOk(): void {
+    this.dialogRef.close(true);
   }
 
 }
