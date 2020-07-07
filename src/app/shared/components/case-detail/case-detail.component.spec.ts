7ef9f533514e2f9d9adcde4890506e5bedd01999
pragmaExito <israel.tarazona@pragma.com.co>
2020-07-06 19:01:05 -0500

    no message

--- src/app/shared/components/case-detail/case-detail.component.spec.ts
+++ src/app/shared/components/case-detail/case-detail.component.spec.ts
@@ -1,128 +1,128 @@
-// import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-// import { CaseDetailComponent } from "./case-detail.component";
-// import { BasicCardComponent } from "../basic-card/basic-card.component";
-// import { TranslateModule } from "@ngx-translate/core";
-// import { ConversationMessageComponent } from "../conversation-message/conversation-message.component";
-// import { ConversationComponent } from "../conversation/conversation.component";
-// import { CommonService } from "@app/shared/services/common.service";
-// import { MatIconModule, MatDialogModule, MatSnackBarModule } from "@angular/material";
-// import { HttpClientTestingModule } from "@angular/common/http/testing";
-// import { EndpointService } from "@app/core";
+import { CaseDetailComponent } from "./case-detail.component";
+import { BasicCardComponent } from "../basic-card/basic-card.component";
+import { TranslateModule } from "@ngx-translate/core";
+import { ConversationMessageComponent } from "../conversation-message/conversation-message.component";
+import { ConversationComponent } from "../conversation/conversation.component";
+import { CommonService } from "@app/shared/services/common.service";
+import { MatIconModule, MatDialogModule, MatSnackBarModule } from "@angular/material";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { EndpointService } from "@app/core";
 
-// describe("CaseDetailComponent", () => {
-//   let component: CaseDetailComponent;
-//   let fixture: ComponentFixture<CaseDetailComponent>;
+describe("CaseDetailComponent", () => {
+  let component: CaseDetailComponent;
+  let fixture: ComponentFixture<CaseDetailComponent>;
 
-//   const mockEndpointService = <EndpointService>{
-//     get: (name: string, params?: any[], version: string = null): string => ''
-//   };
+  const mockEndpointService = <EndpointService>{
+    get: (name: string, params?: any[], version: string = null): string => ''
+  };
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         CaseDetailComponent,
-//         BasicCardComponent,
-//         ConversationMessageComponent,
-//         ConversationComponent
-//       ],
-//       imports: [
-//         MatIconModule,
-//         MatDialogModule,
-//         MatSnackBarModule,
-//         TranslateModule.forRoot({}),
-//         HttpClientTestingModule
-//       ],
-//       providers: [
-//         CommonService,
-//         { provide: EndpointService, useValue: mockEndpointService },
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CaseDetailComponent,
+        BasicCardComponent,
+        ConversationMessageComponent,
+        ConversationComponent
+      ],
+      imports: [
+        MatIconModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        TranslateModule.forRoot({}),
+        HttpClientTestingModule
+      ],
+      providers: [
+        CommonService,
+        { provide: EndpointService, useValue: mockEndpointService },
 
-//       ]
-//     }).compileComponents();
-//   }));
+      ]
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CaseDetailComponent);
-//     component = fixture.componentInstance;
-//     component.case = {
-//       id: "string",
-//       sellerId: "string",
-//       caseId: "string",
-//       status: "Cerrado",
-//       orderNumber: "string",
-//       reasonPQR: "string",
-//       reasonDetail: "string",
-//       description: "string",
-//       descriptionSolution: "string",
-//       createDate: "string",
-//       updateDate: "string",
-//       customerEmail: "string",
-//       read: false,
-//       follow: [],
-//       attachments: [],
-//       caseNumber: "string",
-//     }
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CaseDetailComponent);
+    component = fixture.componentInstance;
+    component.case = {
+      id: "string",
+      sellerId: "string",
+      caseId: "string",
+      status: "Cerrado",
+      orderNumber: "string",
+      reasonPQR: "string",
+      reasonDetail: "string",
+      description: "string",
+      descriptionSolution: "string",
+      createDate: "string",
+      updateDate: "string",
+      customerEmail: "string",
+      read: false,
+      follow: [],
+      attachments: [],
+      caseNumber: "string",
+    }
+    fixture.detectChanges();
+  });
 
-//   it("should create", () => {
-//     expect(component).toBeTruthy();
-//   });
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it("should isClosed to be true", () => {
-//     expect(component.isClosed).toBeTruthy();
-//   });
+  it("should isClosed to be true", () => {
+    expect(component.isClosed).toBeTruthy();
+  });
 
-//   it("should emit reply event", () => {
-//     component.case = {
-//       id: "string",
-//       sellerId: "string",
-//       caseId: "string",
-//       status: "Abierto",
-//       orderNumber: "string",
-//       reasonPQR: "string",
-//       reasonDetail: "string",
-//       description: "string",
-//       descriptionSolution: "string",
-//       createDate: "string",
-//       updateDate: "string",
-//       customerEmail: "string",
-//       read: false,
-//       follow: [],
-//       attachments: [],
-//       caseNumber: "string",
-//     }
-//     fixture.detectChanges()
-//     component.ngOnInit();
-//     const emit = spyOn(component.reply, 'emit');
-//     component.openResponseDialog()
-//     expect(component.isClosed).toBeFalsy();
-//     expect(emit).toHaveBeenCalled();
-//   });
+  it("should emit reply event", () => {
+    component.case = {
+      id: "string",
+      sellerId: "string",
+      caseId: "string",
+      status: "Abierto",
+      orderNumber: "string",
+      reasonPQR: "string",
+      reasonDetail: "string",
+      description: "string",
+      descriptionSolution: "string",
+      createDate: "string",
+      updateDate: "string",
+      customerEmail: "string",
+      read: false,
+      follow: [],
+      attachments: [],
+      caseNumber: "string",
+    }
+    fixture.detectChanges()
+    component.ngOnInit();
+    const emit = spyOn(component.reply, 'emit');
+    component.openResponseDialog()
+    expect(component.isClosed).toBeFalsy();
+    expect(emit).toHaveBeenCalled();
+  });
 
-//   it("should isClosed to be false", () => {
-//     component.case = {
-//       id: "string",
-//       sellerId: "string",
-//       caseId: "string",
-//       status: "Abierto",
-//       orderNumber: "string",
-//       reasonPQR: "string",
-//       reasonDetail: "string",
-//       description: "string",
-//       descriptionSolution: "string",
-//       createDate: "string",
-//       updateDate: "string",
-//       customerEmail: "string",
-//       read: false,
-//       follow: [],
-//       attachments: [],
-//       caseNumber: "string",
-//     }
-//     fixture.detectChanges()
-//     component.ngOnInit();
-//     expect(component.isClosed).toBeFalsy();
-//   });
+  it("should isClosed to be false", () => {
+    component.case = {
+      id: "string",
+      sellerId: "string",
+      caseId: "string",
+      status: "Abierto",
+      orderNumber: "string",
+      reasonPQR: "string",
+      reasonDetail: "string",
+      description: "string",
+      descriptionSolution: "string",
+      createDate: "string",
+      updateDate: "string",
+      customerEmail: "string",
+      read: false,
+      follow: [],
+      attachments: [],
+      caseNumber: "string",
+    }
+    fixture.detectChanges()
+    component.ngOnInit();
+    expect(component.isClosed).toBeFalsy();
+  });
 
-// });
+});
 
