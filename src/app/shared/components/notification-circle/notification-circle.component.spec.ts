720cdd3c9f7c5356b0c747a90d24c025d7f0e4f4
pragmaExito <israel.tarazona@pragma.com.co>
2020-07-06 20:08:51 -0500

    no message

--- src/app/shared/components/notification-circle/notification-circle.component.spec.ts
+++ src/app/shared/components/notification-circle/notification-circle.component.spec.ts
@@ -1,37 +1,37 @@
-// import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
-// import { NotificationCircleComponent } from './notification-circle.component';
+import { NotificationCircleComponent } from './notification-circle.component';
 
-// describe('NotificationCircleComponent', () => {
-//   let component: NotificationCircleComponent;
-//   let fixture: ComponentFixture<NotificationCircleComponent>;
+describe('NotificationCircleComponent', () => {
+  let component: NotificationCircleComponent;
+  let fixture: ComponentFixture<NotificationCircleComponent>;
 
-//   beforeEach(fakeAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [NotificationCircleComponent]
-//     }).compileComponents();
-//   }));
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [NotificationCircleComponent]
+    }).compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(NotificationCircleComponent);
-//     component = fixture.componentInstance;
-//     component.value = 1;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotificationCircleComponent);
+    component = fixture.componentInstance;
+    component.value = 1;
+    fixture.detectChanges();
+  });
 
-//   it('should create', (done) => {
-//     expect(component).toBeTruthy();
-//     expect(
-//       fixture.nativeElement.querySelector('#notification-value').innerText
-//     ).toEqual('1');
-//     done();
-//   });
+  it('should create', (done) => {
+    expect(component).toBeTruthy();
+    expect(
+      fixture.nativeElement.querySelector('#notification-value').innerText
+    ).toEqual('1');
+    done();
+  });
 
-//   it('When notifications value is zero', () => {
-//     component.value = 0;
-//     fixture.detectChanges();
-//     expect(fixture.nativeElement.querySelector('.notification-circle')).toEqual(
-//       null
-//     );
-//   });
-// });
+  it('When notifications value is zero', () => {
+    component.value = 0;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.notification-circle')).toEqual(
+      null
+    );
+  });
+});
