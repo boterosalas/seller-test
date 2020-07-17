ea2fcdb204ba590de89de6c8322ab984a017ab64
pragmaExito <israel.tarazona@pragma.com.co>
2020-07-17 16:28:00 -0500

    traducciones para devoluciones comentarios

--- src/app/secure/orders/in-validation/view-comment/view-comment.component.ts
+++ src/app/secure/orders/in-validation/view-comment/view-comment.component.ts
@@ -1,5 +1,5 @@
 /* 3rd party components */
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 import { Component, Inject, OnInit } from '@angular/core';
 // Load the full build.
 import * as _ from 'lodash';
@@ -8,6 +8,7 @@ import * as _ from 'lodash';
 import { FAKE, OrderDevolutionsModel } from '@app/shared';
 import { Subject } from 'rxjs';
 import { InValidationService } from '../in-validation.service';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -20,21 +21,21 @@ export class ViewCommentComponent implements OnInit {
   // Order information
   order: OrderDevolutionsModel;
   processFinish$ = new Subject<any>();
+  public load = true;
+  public snackBar: MatSnackBar;
 
   constructor(
     public dialogRef: MatDialogRef<ViewCommentComponent>,
     public inValidationService: InValidationService,
+    private languageService: TranslateService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     // _.cloneDeep permite clonar el json y no generar error de binding en la vista orders-list,
     // ya que al usar el mimso json estaba presentando cambios en ambas vistas
-    this.order = _.cloneDeep(data.order);
-
-    this.order = this.order || FAKE.FAKEPENDINGDEVOLUTION;
+    this.getAllCommentRefuse();
   }
 
   ngOnInit() {
-    this.getAllCommentRefuse();
   }
 
   getAllCommentRefuse() {
@@ -44,8 +45,17 @@ export class ViewCommentComponent implements OnInit {
     };
     this.inValidationService.getAllCommentRefuse(params).subscribe((res: any) => {
       if (res) {
+        this.load = false;
+        this.order = _.cloneDeep(res);
+        this.order = this.order || FAKE.FAKEPENDINGDEVOLUTION;
         this.processFinish$.next(res);
-      }
+      } else {
+          this.load = false;
+          this.snackBar.open(this.languageService.instant('secure.orders.send.error_ocurred_processing'), this.languageService.instant('actions.close'), {
+            duration: 3000,
+          });
+          this.onNoClick();
+        }
     });
   }
 
--- src/app/secure/orders/pending-devolution/view-comment/view-comment.component.ts
+++ src/app/secure/orders/pending-devolution/view-comment/view-comment.component.ts
@@ -1,11 +1,12 @@
 /* 3rd party components */
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {MatDialogRef, MAT_DIALOG_DATA, MatSnackBar} from '@angular/material';
 import {Component, Inject, OnInit} from '@angular/core';
 import * as _ from 'lodash';
 /* our own custom components */
 import { OrderDevolutionsModel, FAKE } from '@app/shared';
 import { Subject } from 'rxjs';
 import { PendingDevolutionService } from '../pending-devolution.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-view-comment',
@@ -17,22 +18,22 @@ export class ViewCommentComponent implements OnInit {
 
   // Order information
   order: OrderDevolutionsModel;
+  public load = true;
+  public snackBar: MatSnackBar;
   processFinish$ = new Subject<any>();
 
   constructor(
     private pendingDevolutionService: PendingDevolutionService,
+    private languageService: TranslateService,
     public dialogRef: MatDialogRef<ViewCommentComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     // _.cloneDeep permite clonar el json y no generar error de binding en la vista orders-list, ya que al usar
     // el mimso json estaba presentando cambios en ambas vistas
-    this.order = _.cloneDeep(data.order);
-
-    this.order = this.order || FAKE.FAKEPENDINGDEVOLUTION;
+    this.getAllCommentRefuse();
   }
 
   ngOnInit() {
-    this.getAllCommentRefuse();
   }
 
   getAllCommentRefuse() {
@@ -42,7 +43,16 @@ export class ViewCommentComponent implements OnInit {
     };
     this.pendingDevolutionService.getAllCommentRefuse(params).subscribe((res: any) => {
       if (res) {
+        this.load = false;
+        this.order = _.cloneDeep(res);
+        this.order = this.order || FAKE.FAKEPENDINGDEVOLUTION;
         this.processFinish$.next(res);
+      } else {
+        this.load = false;
+        this.snackBar.open(this.languageService.instant('secure.orders.send.error_ocurred_processing'), this.languageService.instant('actions.close'), {
+          duration: 3000,
+        });
+        this.onNoClick();
       }
     });
   }
