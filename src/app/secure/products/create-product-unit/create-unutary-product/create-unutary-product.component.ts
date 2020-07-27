a3a3a73b0813be8be887889ffb3e295dc7d39f70
Papi-Banguera <jose.banguera@pragma.com.co>
2020-07-27 17:05:49 -0500

    edit pending product

--- src/app/secure/products/create-product-unit/create-unutary-product/create-unutary-product.component.ts
+++ src/app/secure/products/create-product-unit/create-unutary-product/create-unutary-product.component.ts
@@ -10,6 +10,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class CreateUnutaryProductComponent implements OnInit {
   ean: string;
   reference: any;
+  pendingProduct: any;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -20,7 +21,9 @@ export class CreateUnutaryProductComponent implements OnInit {
       if ( params['ean'] != null) {
         this.ean = params['ean'];
         this.reference = params['reference'];
+        this.pendingProduct = params['pendingProduct'];
       }
+      console.log(10, this.pendingProduct);
     });
   }
 
