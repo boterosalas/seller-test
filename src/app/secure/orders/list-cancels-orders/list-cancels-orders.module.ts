1b3445bd744a5a919d7de1e04e334a09c01c3188
Papi-Banguera <jose.banguera@pragma.com.co>
2020-10-02 17:26:49 -0500

    start hu

--- src/app/secure/orders/list-cancels-orders/list-cancels-orders.module.ts
+++ src/app/secure/orders/list-cancels-orders/list-cancels-orders.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from '@app/shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { ListCancelsOrdersComponent } from './list-cancels-orders.component';
 import { ListCancelOrderRoutingModule } from './list-cancels-orders.routing';
 
@@ -17,7 +19,9 @@ import { ListCancelOrderRoutingModule } from './list-cancels-orders.routing';
         SharedModule,
         HttpClientModule,
         BrowserAnimationsModule,
-        ListCancelOrderRoutingModule
+        ListCancelOrderRoutingModule,
+        TranslateModule,
+        MatTabsModule
     ],
     declarations: [
         // ListCancelsOrdersComponent
@@ -29,6 +33,8 @@ import { ListCancelOrderRoutingModule } from './list-cancels-orders.routing';
 
     ],
     providers: [
-    ]
+    ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+
 })
 export class ListCancelsOrdersModule { }
