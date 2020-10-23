fa0272010f90f0a26056064944f7f40968fb3b21
Papi-Banguera <jose.banguera@pragma.com.co>
2020-10-23 17:32:28 -0500

    P unitarias descagableeee

--- src/app/secure/orders/list-cancels-orders/list-cancels-orders.component.spec.ts
+++ src/app/secure/orders/list-cancels-orders/list-cancels-orders.component.spec.ts
@@ -1,25 +1,25 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ListCancelsOrdersComponent } from './list-cancels-orders.component';
+import { ListCancelsOrdersComponent } from './list-cancels-orders.component';
 
-// describe('ListCancelsOrdersComponent', () => {
-//   let component: ListCancelsOrdersComponent;
-//   let fixture: ComponentFixture<ListCancelsOrdersComponent>;
+describe('ListCancelsOrdersComponent', () => {
+  let component: ListCancelsOrdersComponent;
+  let fixture: ComponentFixture<ListCancelsOrdersComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ListCancelsOrdersComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListCancelsOrdersComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ListCancelsOrdersComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListCancelsOrdersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
