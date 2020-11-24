477070c9204322029f01c46f3c097b266c523efb
pragmaExito <israel.tarazona@pragma.com.co>
2020-11-24 17:47:44 -0500

    no message

--- src/app/secure/reports/report-commission/report-commission.component.spec.ts
+++ src/app/secure/reports/report-commission/report-commission.component.spec.ts
@@ -1,20 +1,65 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '@app/material.module';
 import { ReportCommissionComponent } from './report-commission.component';
+import { SharedModule } from '@app/shared/shared.module';
+import { StoresService } from '@app/secure/offers/stores/stores.service';
+import { CognitoUtil, EndpointService, LoadingService, UserLoginService, UserParametersService } from '@app/core';
+import { SupportService } from '@app/secure/support-modal/support.service';
+import { ShellComponent } from '@app/core/shell';
+import { ComponentsService, EventEmitterOrders } from '@app/shared';
+import { of } from 'rxjs';
 
-describe('ReportCommissionComponent', () => {
+fdescribe('ReportCommissionComponent', () => {
+
+  const mockLoadingService = jasmine.createSpyObj('LoadingService', ['viewSpinner', 'closeSpinner']);
+  const mockUserLoginService = jasmine.createSpyObj('UserLoginService', ['isAuthenticated']);
+  const mockStoresService = jasmine.createSpyObj('StoresService', ['getAllStoresFull']);
   let component: ReportCommissionComponent;
   let fixture: ComponentFixture<ReportCommissionComponent>;
 
+  const response = {
+    data: true,
+    errors: [],
+    message: null,
+    pendingResponse: false,
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReportCommissionComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MaterialModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        SharedModule
+      ],
+      declarations: [ ReportCommissionComponent ],
+      providers: [
+        {provide : StoresService , useValue: mockStoresService},
+        EndpointService,
+        UserParametersService,
+        { provide: LoadingService, useValue: mockLoadingService },
+        { provide: UserLoginService, useValue: mockUserLoginService },
+        SupportService,
+        ShellComponent,
+        ComponentsService,
+        EventEmitterOrders,
+        CognitoUtil
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ReportCommissionComponent);
+    mockStoresService.getAllStoresFull.and.returnValue(of(response));
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
