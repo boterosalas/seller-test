deddf978380de8772a4a610aa304db0294f3169c
Papi-Banguera <jose.banguera@pragma.com.co>
2020-12-04 16:18:45 -0500

    Start hu 3

--- src/app/secure/dispersion/detail-payment/detail-payment.component.ts
+++ src/app/secure/dispersion/detail-payment/detail-payment.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { LoadingService } from '@app/core';
 import { EventEmitterSeller } from '@app/shared/events/eventEmitter-seller.service';
 
 @Component({
@@ -7,18 +8,36 @@ import { EventEmitterSeller } from '@app/shared/events/eventEmitter-seller.servi
   styleUrls: ['./detail-payment.component.scss']
 })
 export class DetailPaymentComponent implements OnInit {
-
-  @Output() sellerDataSearch = new EventEmitter();
+  sellerData: any;
+  activeTabs: Boolean = false;
+  // @Input() sellerData: any;
 
 
   constructor(
     private emitterSeller: EventEmitterSeller,
+    private loadingService?: LoadingService,
+
   ) { }
 
   ngOnInit() {
+    this.selectSeller();
+    // this.loadingService.viewSpinner(); // Mostrar el spinner
+    // this.emitterSeller.eventSearchSeller.subscribe(data => {
+    //   this.sellerData = data;
+    //   this.loadingService.viewSpinner(); // Mostrar el spinner
+    // });
+  }
+
+  selectSeller() {
+    this.loadingService.viewSpinner();
     this.emitterSeller.eventSearchSeller.subscribe(data => {
-      console.log('dayta: ', data);
-      this.sellerDataSearch.emit(data);
+      if (data) {
+        this.activeTabs = true;
+        this.sellerData = data;
+      } else {
+        this.activeTabs = false;
+      }
+      this.loadingService.closeSpinner();
     });
   }
 
--- src/app/secure/dispersion/detail-payment/historical-payment/historical-payment.component.ts
+++ src/app/secure/dispersion/detail-payment/historical-payment/historical-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-historical-payment',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoricalPaymentComponent implements OnInit {
 
+  @Input() sellerData: any;
+
+
   constructor() { }
 
   ngOnInit() {
+    console.log(1, this.sellerData);
+
   }
 
 }
