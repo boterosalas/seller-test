dc9b87efb1f032237ec77136f70b48c5f7fb985f
Papi-Banguera <jose.banguera@pragma.com.co>
2020-12-11 23:42:25 -0500

    Parametros del filtro historico

--- src/app/secure/dispersion/detail-payment/historical-payment/historical-payment.component.ts
+++ src/app/secure/dispersion/detail-payment/historical-payment/historical-payment.component.ts
@@ -34,7 +34,7 @@ export class HistoricalPaymentComponent implements OnInit {
   public onlyOne = true;
   public length = 0;
   // public limit = 50;
-  public limit = 1;
+  public limit = 2;
   public paginationToken = '{}';
   public filterHistoricalPayment: FormGroup;
   public btnFilter = false;
@@ -46,9 +46,6 @@ export class HistoricalPaymentComponent implements OnInit {
   @ViewChild('sidenavHistoricalPayment') sidenavHistoricalPayment: MatSidenav;
   @ViewChild('toolbarOptions') toolbarOption;
   urlParams: string;
-
-
-
 
   public informationToForm: SearchFormEntity = {
     title: 'module.Dispersion',
@@ -75,6 +72,10 @@ export class HistoricalPaymentComponent implements OnInit {
     this.getAllListHiistoric();
   }
 
+  /**
+   * Metodo para crear el formulario del filtro 
+   * @memberof HistoricalPaymentComponent
+   */
   createFormControls() {
     this.filterHistoricalPayment = this.fb.group({
       cutOffDate: new FormControl(''),
@@ -101,15 +102,9 @@ export class HistoricalPaymentComponent implements OnInit {
     console.log(event);
     const newLimit = event.param.pageSize;
     const index = event.param.pageIndex - 1;
-    console.log('index: ', index);
-    console.log('event.param.pageIndex: ', event.param.pageIndex);
-    console.log('this.arrayPosition: ', this.arrayPosition);
     if (newLimit !== this.limit) {
-      console.log('es diferente');
       this.indexPage = 0;
       this.limit = event.param.pageSize;
-      // this.paginationToken = '{}';
-      // this.limit = 50;
       this.currentPage = 0;
       const paginator = this.toolbarOption.getPaginator();
       paginator.pageIndex = 0;
@@ -117,78 +112,143 @@ export class HistoricalPaymentComponent implements OnInit {
       this.arrayPosition.push('{}');
     } else {
       let newPaginationToken = this.arrayPosition[index];
-      console.log('here', newPaginationToken, this.arrayPosition[index]);
-
       if (newPaginationToken === undefined) {
-        console.log('here 2');
         newPaginationToken = '{}';
       }
       this.paginationToken = newPaginationToken;
     }
-    console.log(this.paginationToken);
     this.urlParams = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
-    this.getAllListHiistoric(this.urlParams);
+    this.getAllListHiistoric(this.urlParams, null);
+  }
+
+  /**
+   * Metodo para validar el formato de la fecha dd-mm-yyyy
+   * @param {*} date
+   * @returns {*}
+   * @memberof HistoricalPaymentComponent
+   */
+  public getDate(date: any): any {
+    const day = this.addsZeroDate(date.getDate().toString());
+    const months = this.addsZeroDate((date.getMonth() + 1).toString());
+    const year = date.getFullYear();
+    return year + '-' + months + '-' + day;
+  }
+
+  /**
+   * Funcion para agregar 0 a la fecha si lo necesita
+   * @param {*} param
+   * @returns {*}
+   * @memberof HistoricalPaymentComponent
+   */
+  public addsZeroDate(param: any): any {
+    if (param.length < 2) {
+      return '0' + param;
+    }
+    return param;
   }
 
   /**
    * Funcion para traer el listado del historico de pagos
    * @memberof HistoricalPaymentComponent
    */
-  getAllListHiistoric(aplyFilter?: any) {
-    console.log('entró');
+  getAllListHiistoric(applyPagination?: any, paramsFilter?: any) {
     let url;
+    let urlFilters;
     this.loadingService.viewSpinner();
-    console.log(1, aplyFilter);
-    if (aplyFilter) {
-      console.log('tiene');
-      url = this.urlParams;
-    } else {
-      console.log('no tiene');
-      url = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
-      // this.urlParams = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
-    }
-    const urlFilters = {
-      SellerId: this.sellerData.IdSeller.toString(),
-      DispersionFilter: {}
-    };
-    this.detailPaymentService.getAllDetailPayment(url, urlFilters).subscribe((res: any) => {
-      if (res && res.status === 200) {
-        const { viewModel, count, paginationToken } = res.body;
-        // console.log(this.statusAllCheck);
-        this.dataSource = new MatTableDataSource(viewModel);
-        if (this.onlyOne) {
-          this.length = count;
+    console.log(1, applyPagination);
+    if (applyPagination || paramsFilter) {
+      url = paramsFilter ? `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}` : this.urlParams;
+      if (paramsFilter) {
+        urlFilters = {
+          SellerId: this.sellerData.IdSeller.toString(),
+          DispersionFilter: {
+            CutOffDate: paramsFilter.cutOffDate,
+            DispersionDate: paramsFilter.dispersionDate,
+            InternalPaymentId: paramsFilter.internalIdPayment,
+            OrderNumber: paramsFilter.orderNumber
+          }
+        };
+       } else {
+          urlFilters = {
+            SellerId: this.sellerData.IdSeller.toString(),
+            DispersionFilter: {}
+          };
         }
-        this.onlyOne = false;
-        this.loadingService.closeSpinner();
-        console.log(33, paginationToken);
-        this.savePaginationToken(paginationToken);
       } else {
+        url = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
+        urlFilters = {
+          SellerId: this.sellerData.IdSeller.toString(),
+          DispersionFilter: {}
+        };
+      }
+      this.detailPaymentService.getAllDetailPayment(url, urlFilters).subscribe((res: any) => {
+        if (res && res.status === 200) {
+          const { viewModel, count, paginationToken } = res.body;
+          // console.log(this.statusAllCheck);
+          this.dataSource = new MatTableDataSource(viewModel);
+          if (this.onlyOne) {
+            this.length = count;
+          }
+          this.onlyOne = false;
+          this.loadingService.closeSpinner();
+          console.log(33, paginationToken);
+          this.savePaginationToken(paginationToken);
+          this.stateSideNavOrder = false;
+        } else {
+          this.loadingService.closeSpinner();
+          this.snackBar.open(this.languageService.instant('public.auth.forgot.error_try_again'), this.languageService.instant('actions.close'), {
+            duration: 3000,
+          });
+        }
+      }, error => {
         this.loadingService.closeSpinner();
         this.snackBar.open(this.languageService.instant('public.auth.forgot.error_try_again'), this.languageService.instant('actions.close'), {
           duration: 3000,
         });
-      }
-    }, error => {
-      this.loadingService.closeSpinner();
-      this.snackBar.open(this.languageService.instant('public.auth.forgot.error_try_again'), this.languageService.instant('actions.close'), {
-        duration: 3000,
       });
-    });
-  }
-
-  /**
-   * Funcion para guardar el paginationtoken
-   * @param {string} pagination
-   * @memberof HistoricalPaymentComponent
-   */
-  savePaginationToken(pagination: string) {
-    const isExist = this.arrayPosition.includes(pagination);
-    if (isExist === false) {
-      this.arrayPosition.push(pagination);
     }
-    console.log(33, pagination);
+
+    /**
+     * Funcion para guardar el paginationtoken
+     * @param {string} pagination
+     * @memberof HistoricalPaymentComponent
+     */
+    savePaginationToken(pagination: string) {
+      const isExist = this.arrayPosition.includes(pagination);
+      if (isExist === false) {
+        this.arrayPosition.push(pagination);
+      }
+    }
+
+    /**
+     * Metodo para aplicar filtros
+     * @param {*} params
+     * @memberof HistoricalPaymentComponent
+     */
+    apllyFilterHistoricalPayment(params: any) {
+      if (params && params.cutOffDate) {
+        params.cutOffDate = this.getDate(new Date(params.cutOffDate));
+      }
+      if (params && params.dispersionDate) {
+        params.dispersionDate = this.getDate(new Date(params.dispersionDate));
+      }
+      this.getAllListHiistoric(null, params);
+      this.toggleFilterHistoricalPayment();
+      this.onlyOne = true;
+    }
+
+    /**
+     * Boton limpiar formulario, refresca data sin filtros
+     * @memberof HistoricalPaymentComponent
+     */
+    clearForm() {
+      this.onlyOne = true;
+      this.filterHistoricalPayment.reset();
+      this.arrayPosition = [];
+      this.paginationToken = '{}';
+      this.currentPage = 0;
+      this.arrayPosition.push('{}');
+      this.getAllListHiistoric();
+    }
 
   }
-
-}
