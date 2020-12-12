69b74628940fc12bf0ff6f0b988eb52947eb1372
Papi-Banguera <jose.banguera@pragma.com.co>
2020-12-11 20:55:11 -0500

    pagintation start 1

--- src/app/secure/dispersion/detail-payment/historical-payment/historical-payment.component.ts
+++ src/app/secure/dispersion/detail-payment/historical-payment/historical-payment.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { MatSnackBar, MatTableDataSource } from '@angular/material';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSidenav, MatSnackBar, MatTableDataSource } from '@angular/material';
 import { LoadingService } from '@app/core';
 import { SupportService } from '@app/secure/support-modal/support.service';
+import { InformationToForm, SearchFormEntity } from '@app/shared';
 import { TranslateService } from '@ngx-translate/core';
 import { DetailPaymentService } from '../detail-payment.service';
 
@@ -28,10 +29,36 @@ export class HistoricalPaymentComponent implements OnInit {
 
   public arrayPosition = [];
   public dataSource: MatTableDataSource<any>;
+
+
   public onlyOne = true;
   public length = 0;
-  public limit = 50;
+  // public limit = 50;
+  public limit = 1;
   public paginationToken = '{}';
+  public filterHistoricalPayment: FormGroup;
+  public btnFilter = false;
+  public indexPage = 0;
+  public currentPage = 0;
+  public stateSideNavOrder = false;
+
+
+  @ViewChild('sidenavHistoricalPayment') sidenavHistoricalPayment: MatSidenav;
+  @ViewChild('toolbarOptions') toolbarOption;
+  urlParams: string;
+
+
+
+
+  public informationToForm: SearchFormEntity = {
+    title: 'module.Dispersion',
+    subtitle: 'menu.Resumen de pagos',
+    btn_title: 'dispersión',
+    title_for_search: 'Filtro histórico de pagos',
+    type_form: 'module',
+    information: new InformationToForm,
+    count: null
+  };
 
   constructor(
     private snackBar: MatSnackBar,
@@ -44,23 +71,87 @@ export class HistoricalPaymentComponent implements OnInit {
 
   ngOnInit() {
     console.log(1, this.sellerData);
+    this.createFormControls();
     this.getAllListHiistoric();
+  }
+
+  createFormControls() {
+    this.filterHistoricalPayment = this.fb.group({
+      cutOffDate: new FormControl(''),
+      dispersionDate: new FormControl(''),
+      internalIdPayment: new FormControl(''),
+      orderNumber: new FormControl('')
+    });
+  }
+
+  /**
+   * Función para abrir el toogle del filtro
+   * @memberof HistoricalPaymentComponent
+   */
+  toggleFilterHistoricalPayment() {
+    this.sidenavHistoricalPayment.toggle();
+  }
+
+  /**
+   * Metodo para ir guardando el paginationToken y manejar paginado
+   * @param {*} event
+   * @memberof HistoricalPaymentComponent
+   */
+  paginations(event: any) {
+    console.log(event);
+    const newLimit = event.param.pageSize;
+    const index = event.param.pageIndex - 1;
+    console.log('index: ', index);
+    console.log('event.param.pageIndex: ', event.param.pageIndex);
+    console.log('this.arrayPosition: ', this.arrayPosition);
+    if (newLimit !== this.limit) {
+      console.log('es diferente');
+      this.indexPage = 0;
+      this.limit = event.param.pageSize;
+      // this.paginationToken = '{}';
+      // this.limit = 50;
+      this.currentPage = 0;
+      const paginator = this.toolbarOption.getPaginator();
+      paginator.pageIndex = 0;
+      this.arrayPosition = [];
+      this.arrayPosition.push('{}');
+    } else {
+      let newPaginationToken = this.arrayPosition[index];
+      console.log('here', newPaginationToken, this.arrayPosition[index]);
+
+      if (newPaginationToken === undefined) {
+        console.log('here 2');
+        newPaginationToken = '{}';
+      }
+      this.paginationToken = newPaginationToken;
+    }
+    console.log(this.paginationToken);
+    this.urlParams = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
+    this.getAllListHiistoric(this.urlParams);
   }
 
   /**
    * Funcion para traer el listado del historico de pagos
    * @memberof HistoricalPaymentComponent
    */
-  getAllListHiistoric() {
+  getAllListHiistoric(aplyFilter?: any) {
     console.log('entró');
-    const url = 'hola';
+    let url;
     this.loadingService.viewSpinner();
-    const urlParams = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
+    console.log(1, aplyFilter);
+    if (aplyFilter) {
+      console.log('tiene');
+      url = this.urlParams;
+    } else {
+      console.log('no tiene');
+      url = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
+      // this.urlParams = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`;
+    }
     const urlFilters = {
       SellerId: this.sellerData.IdSeller.toString(),
       DispersionFilter: {}
     };
-    this.detailPaymentService.getAllDetailPayment(urlParams, urlFilters).subscribe((res: any) => {
+    this.detailPaymentService.getAllDetailPayment(url, urlFilters).subscribe((res: any) => {
       if (res && res.status === 200) {
         const { viewModel, count, paginationToken } = res.body;
         // console.log(this.statusAllCheck);
@@ -70,6 +161,7 @@ export class HistoricalPaymentComponent implements OnInit {
         }
         this.onlyOne = false;
         this.loadingService.closeSpinner();
+        console.log(33, paginationToken);
         this.savePaginationToken(paginationToken);
       } else {
         this.loadingService.closeSpinner();
@@ -95,6 +187,8 @@ export class HistoricalPaymentComponent implements OnInit {
     if (isExist === false) {
       this.arrayPosition.push(pagination);
     }
+    console.log(33, pagination);
+
   }
 
 }
