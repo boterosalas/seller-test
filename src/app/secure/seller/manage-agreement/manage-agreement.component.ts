7c48c3dbc5f9ab2f9b4454ba4b36d2450a1ca770
Papi-Banguera <jose.banguera@pragma.com.co>
2021-02-25 20:50:58 -0500

    Paginacion OK

--- src/app/secure/seller/manage-agreement/manage-agreement.component.ts
+++ src/app/secure/seller/manage-agreement/manage-agreement.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, PageEvent } from '@angular/material';
 import { LoadingService, Logger, ModalService } from '@app/core';
 import { SellerService } from '../seller.service';
 import { ModalBulkloadAgreementComponent } from './modal-bulkload-agreement/modal-bulkload-agreement.component';
@@ -18,6 +18,15 @@ export class ManageAgreementComponent implements OnInit {
   public paginationToken = '{}';
   public limit = 0;
   titleAgreement: any;
+  length = 0;
+  public pageSize = 3;
+
+  public arrayPosition = [];
+  paramsArray: any;
+  pageSizeOptions: number[] = [3, 6, 9];
+  pageEvent: PageEvent;
+  public callOne = true;
+
 
   constructor(
     private dialog: MatDialog,
@@ -38,38 +47,68 @@ export class ManageAgreementComponent implements OnInit {
     const dialogRef = this.dialog.open(ModalBulkloadAgreementComponent, {
       width: '60%',
       minWidth: '280px',
-      data: { }
+      data: {}
     });
     dialogRef.afterClosed().subscribe(result => {
       log.info('The modal detail billing was closed');
     });
   }
 
-  public getAllBrands() {
+  public getAllBrands(params?: any) {
     this.loading.viewSpinner();
-    const urlParams = `?limit=${this.limit}&paginationToken=${encodeURI(this.paginationToken)}`
+    let urlParams;
+    if (params) {
+      urlParams = params;
+    } else {
+      urlParams = `?limit=${this.pageSize}&paginationToken=${encodeURI(this.paginationToken)}`
+    }
     this.sellerService.getAllAgreement(urlParams).subscribe((result: any) => {
-       console.log('result: ', result);
-       if (result ) {
+      console.log('result: ', result);
+      if (result) {
         this.manageAgreementsSeller = result.ViewModel;
         this.titleAgreement = result.ContractName;
+        if (this.callOne) {
+          this.length = result.Count;
+          this.arrayPosition = [];
+          this.arrayPosition.push('{}');
+          this.callOne = false;
+        }
+        this.paginationToken = result.PaginationToken;
         this.loading.closeSpinner();
-       }
-       
-        // if (res && parseInt(res.Total) > 0) {
-        //     this.brandsList = res.Brands;
-        //     this.length = res.Total;
-        //     this.sortedData = this.mapItems(
-        //         res.Brands,
-        //     );
-        // } else {
-        //     this.sortedData = null;
-        // }
-
+      }
     }, error => {
-        this.loading.closeSpinner();
-        this.modalService.showModal('errorService');
+      this.loading.closeSpinner();
+      this.modalService.showModal('errorService');
     });
-}
+  }
+
+  paginations(event: any): any {
+    console.log('6: ', event);
+    if (event.pageSize !== this.limit) {
+      this.limit = event.pageSize;
+    }
+    if (event && event.pageIndex >= 0) {
+      const index = event.pageIndex;
+      if (index === 0) {
+        this.paginationToken = encodeURI('{}');
+      }
+      const isExistInitial = this.arrayPosition.includes('{}');
+      if (isExistInitial === false) {
+        this.arrayPosition.push('{}');
+      }
+      const isExist = this.arrayPosition.includes(this.paginationToken);
+      if (isExist === false) {
+        this.arrayPosition.push(this.paginationToken);
+      }
+      this.paginationToken = this.arrayPosition[index];
+      if (this.paginationToken === undefined) {
+        this.paginationToken = encodeURI('{}');
+      }
+      this.paramsArray = {
+        'limit': this.limit + '&paginationToken=' + this.paginationToken
+      };
+      this.getAllBrands();
+    }
+  }
 
 }
