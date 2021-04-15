88a8659608416adf9ec3673a97f2d46c2826b598
davidbetancur <david.betancur@pragma.com.co>
2021-04-15 17:02:23 -0500

    fraud initial structure

---
+++ src/app/secure/reports/fraud-notification/components/upload-fraud/upload-fraud.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UploadFraudComponent } from './upload-fraud.component';
+
+describe('UploadFraudComponent', () => {
+  let component: UploadFraudComponent;
+  let fixture: ComponentFixture<UploadFraudComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UploadFraudComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UploadFraudComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/secure/reports/fraud-notification/components/upload-fraud/upload-fraud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-fraud',
+  templateUrl: './upload-fraud.component.html',
+  styleUrls: ['./upload-fraud.component.scss']
+})
+export class UploadFraudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
