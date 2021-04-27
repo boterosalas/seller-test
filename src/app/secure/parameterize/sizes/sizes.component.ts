10442a3b74430ee9bda6345b6dc42fe093cfa899
Papi-Banguera <jose.banguera@pragma.com.co>
2021-04-26 19:05:07 -0500

    Vista premilimar OK

--- src/app/secure/parameterize/sizes/sizes.component.ts
+++ src/app/secure/parameterize/sizes/sizes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-sizes',
@@ -17,6 +18,17 @@ export class SizesComponent implements OnInit {
     { Name: 'S', State: true }];
 
 
+  public paginationToken = '{}';
+  public limit = 0;
+  titleAgreement: any;
+  length = 0;
+  public pageSize = 50;
+
+  public arrayPosition = [];
+  paramsArray: any;
+  pageSizeOptions: number[] = [50, 100, 200];
+  pageEvent: PageEvent;
+  public callOne = true;
   constructor() { }
 
   ngOnInit() {
