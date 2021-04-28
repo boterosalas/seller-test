9db337f6d148937727d5d2f93ea082a92eabf5be
pragmaExito <israel.tarazona@pragma.com.co>
2021-04-28 14:12:13 -0500

    no message

--- src/app/secure/parameterize/notification-admin/notification-admin.component.ts
+++ src/app/secure/parameterize/notification-admin/notification-admin.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification-admin.component.scss']
 })
 export class NotificationAdminComponent implements OnInit {
+  
+  public downloadPermission = false;
+  public btnFilter = true;
+  public btnDownload = false;
+  public typeProfile = 3;
+  public length = [];
+  public isClear = false;
+  public isFullSearch = false;
+  public informationToForm: any = {
+    title: 'module.Parametrización',
+    subtitle: 'menu.Notificación',
+    btn_title: '',
+    title_for_search: 'secure.notifications.container.title_for_search',
+    type_form: 'notification',
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  paginations (event: any) {
+
+  }
+
+  changeSizeOrderTable(even: any) {
+
   }
 
 }
