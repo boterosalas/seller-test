7c29c5f6604863e54ddeef829ab76495de3e88e2
pragmaExito <israel.tarazona@pragma.com.co>
2021-04-28 17:06:24 -0500

    listado de anuncios

--- src/app/secure/parameterize/notification-admin/notification-admin.component.ts
+++ src/app/secure/parameterize/notification-admin/notification-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatSidenav, ErrorStateMatcher, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-notification-admin',
@@ -22,17 +23,81 @@ export class NotificationAdminComponent implements OnInit {
     type_form: 'notification',
   };
 
+  public displayedColumns = [
+    'title',
+    'date',
+    'actions'
+  ];
+  public dataSource: MatTableDataSource<any>;
+
+  public mockList = {
+    'Errors': [],
+    'Data': [
+        {
+            'Title': 'Test Esteban',
+            'CreationDate': '2021-03-24T05:46:25.145-05:00',
+            'InitialDate': '2021-03-19T00:00:00-05:00',
+            'FinalDate': null,
+            'Body': null,
+            'UrlImage': null,
+            'BackgroundColor': null,
+            'Target': 'International',
+            'Available': false,
+            'Link': null,
+            'Id': 637521795851451149,
+            'IsNew': false,
+            'NewInitialDate': null
+        },
+        {
+            'Title': 'Test Esteban',
+            'CreationDate': '2021-03-24T05:46:25.145-05:00',
+            'InitialDate': '2021-03-19T00:00:00-05:00',
+            'FinalDate': null,
+            'Body': null,
+            'UrlImage': null,
+            'BackgroundColor': null,
+            'Target': 'International',
+            'Available': false,
+            'Link': null,
+            'Id': 637521795851451149,
+            'IsNew': false,
+            'NewInitialDate': null
+        },
+        {
+            'Title': 'Test Esteban',
+            'CreationDate': '2021-03-24T05:46:25.145-05:00',
+            'InitialDate': '2021-03-19T00:00:00-05:00',
+            'FinalDate': null,
+            'Body': null,
+            'UrlImage': null,
+            'BackgroundColor': null,
+            'Target': 'International',
+            'Available': false,
+            'Link': null,
+            'Id': 637521795851451149,
+            'IsNew': false,
+            'NewInitialDate': null
+        }
+    ],
+    'Message': ''
+};
+
+
+
+
+
+
   constructor() { }
 
   ngOnInit() {
+    this.getAllAdvertisements();
   }
 
-  paginations (event: any) {
-
+  getAllAdvertisements() {
+    this.dataSource = new MatTableDataSource(this.mockList.Data);
   }
 
-  changeSizeOrderTable(even: any) {
-
-  }
+  paginations (event: any) {}
+  changeSizeOrderTable(even: any) {}
 
 }
