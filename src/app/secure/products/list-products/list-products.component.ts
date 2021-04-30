ae3a9c6bfc0efebdc38813ef5cf7c0eca8e83c0b
Papi-Banguera <jose.banguera@pragma.com.co>
2021-04-30 16:01:06 -0500

    delete consoles

--- src/app/secure/products/list-products/list-products.component.ts
+++ src/app/secure/products/list-products/list-products.component.ts
@@ -288,7 +288,6 @@ export class ListProductsComponent implements OnInit {
         } else {
             this.finalDateList = null;
         }
-        console.log(this.keyPlus);
         this.modelDelete = {
             ean: this.eanList || null,
             plu: this.keyPlus ? this.keyPlus.toString() : null,
@@ -337,7 +336,6 @@ export class ListProductsComponent implements OnInit {
     setIntervalStatusDelete() {
         clearInterval(this.checkIfDoneCharge);
         this.checkIfDoneCharge = setInterval(() => this.productsService.verifyStatusDelete().subscribe((res) => {
-            console.log(res);
             this.verifyStateCharge(res);
         }), 7000);
     }
