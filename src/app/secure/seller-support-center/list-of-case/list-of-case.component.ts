a3ce4d678b7c3ece77f54fe19d8b7c0f72639c9b
Papi-Banguera <jose.banguera@pragma.com.co>
2021-06-28 16:55:16 -0500

    Mobile OK

--- src/app/secure/seller-support-center/list-of-case/list-of-case.component.ts
+++ src/app/secure/seller-support-center/list-of-case/list-of-case.component.ts
@@ -144,6 +144,7 @@ export class ListOfCaseComponent implements OnInit, OnDestroy {
   hideHeader:Boolean;
 
   @ViewChild(MatSort , {static: false}) sort: MatSort;
+  timeResponse: any;
 
 
   constructor(
@@ -462,6 +463,8 @@ export class ListOfCaseComponent implements OnInit, OnDestroy {
             this.cases.forEach(element => {
               element.statusLoad = false;
             });
+            this.timeResponse = res.body.data.responseTime;
+            console.log(this.timeResponse, res.body.data);
             this.loadingService.closeSpinner();
           }
         }
