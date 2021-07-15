d6330ac57521adf35ef1dc75ec3fe38c8bd209aa
Papi-Banguera <jose.banguera@pragma.com.co>
2021-07-15 11:09:51 -0500

    Unitarias OK

--- src/app/secure/seller/agreement/agreement.component.spec.ts
+++ src/app/secure/seller/agreement/agreement.component.spec.ts
@@ -7,6 +7,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { LoadingService, UserParametersService } from '@app/core';
 import { MaterialModule } from '@app/material.module';
 import { AuthService } from '@app/secure/auth/auth.routing';
+import { StoresService } from '@app/secure/offers/stores/stores.service';
 import { BillingOrdersService } from '@app/secure/orders/billing-orders/billing-orders.service';
 import { ComponentsService } from '@app/shared';
 import { EventEmitterSeller } from '@app/shared/events/eventEmitter-seller.service';
@@ -21,28 +22,29 @@ fdescribe('AgreementComponent', () => {
     let component: AgreementComponent;
     let fixture: ComponentFixture<AgreementComponent>;
 
-    const dataDelete = {
-        dataAgreement: {
-            DocumentId: '637347387380880022',
-            DocumentType: 2,
-            SellerId: 100005
-        },
-        deleteMultiple: 1,
-        name: 'Acuerdo - qwertyu',
-        // Sellers: [12485],
-        // TypeContracts: '2',
-        // deleteMultiple: 2,
-        // name: 'Acuerdo - Acuerdo victor uno',
-        // DocumentId: '2'
+    const agreementMenu = {
+        Functionalities: [{
+            NameFunctionality: 'Consultar',
+            ShowFunctionality: true,
+            nameFunctionalityBack: 'Consultar'
+        }],
     };
 
+    const reponseUserSYNC = {
+        sellerEmail: 'exitoregresion@gmail.com',
+        sellerId: '11226',
+        sellerName: 'pruebas qa test',
+        sellerNit: '9852410170',
+        sellerProfile: 'seller'
+    };
 
-    const newResponseRegex = {
-        statusCode: 200,
+    const userData = { sellerProfile: 'seller' };
+
+    const mockUser = Object.assign({}, userData);
+
+    const responseGetUser = {
         body: {
-            body: JSON.stringify({
-                Data: true
-            })
+            body: JSON.stringify({ Data: mockUser })
         }
     };
     const mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close', 'afterClosed', 'componentInstance']);
@@ -51,6 +53,7 @@ fdescribe('AgreementComponent', () => {
     const mockAgreementService = jasmine.createSpyObj('AgreementService', ['getAgreements']);
     const mockBillingOrdersService = jasmine.createSpyObj('BillingOrdersService', ['getDownnLoadBilling']);
     const mockAuthService = jasmine.createSpyObj('AuthService', ['getMenu']);
+    const mockUserParameterService = jasmine.createSpyObj('UserParametersService', ['getUserData']);
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -67,7 +70,8 @@ fdescribe('AgreementComponent', () => {
             providers: [
                 ComponentsService,
                 EventEmitterSeller,
-                UserParametersService,
+                StoresService,
+                { provide: UserParametersService, useValue: mockUserParameterService },
                 { provide: MatDialogRef, useValue: mockDialogRef },
                 { provide: LoadingService, useValue: mockLoadingService },
                 { provide: MatDialog, useValue: mockDialog },
@@ -76,21 +80,18 @@ fdescribe('AgreementComponent', () => {
                 { provide: AuthService, useValue: mockAuthService },
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
-        })
-            .compileComponents();
+        }).compileComponents();
     }));
 
     beforeEach(() => {
+        mockAuthService.getMenu.and.returnValue(agreementMenu);
+        mockUserParameterService.getUserData.and.returnValue(of(reponseUserSYNC));
         fixture = TestBed.createComponent(AgreementComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
-        expect(component).toBeTruthy();
-    });
-
-    it('dialog should be closed after onNoClick()', () => {
         expect(component).toBeTruthy();
     });
 });
