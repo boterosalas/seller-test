9edbee39381a4962c4fd2763124b1fdfa549ae24
Jose banguera <jose.banguer@pragma.com.co>
2021-09-02 11:03:08 -0500

    Fix styles anuncios

--- src/app/secure/dashboard/modal-advertisements/modal-advertisements.component.ts
+++ src/app/secure/dashboard/modal-advertisements/modal-advertisements.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-modal-advertisements',
@@ -7,20 +8,32 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
   styleUrls: ['./modal-advertisements.component.scss']
 })
 export class ModalAdvertisementsComponent implements OnInit {
+  body: any;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<ModalAdvertisementsComponent>,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
+    this.setBody();
   }
 
   /**
-   * cierra la modal
+   * Seteo parametros al body con estilos
    */
+  setBody() {
+    if (this.data && this.data.Body) {
+      this.body = this.data.Body ? this.sanitizer.bypassSecurityTrustHtml(this.data.Body) : null;
+    }
+  }
 
-   public close(): void {
+
+  /**
+   * Cierra el modal
+   */
+  public close(): void {
     this.dialogRef.close();
   }
 
